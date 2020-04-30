eb65fe660237da150ca65e000ce26a80a1685431
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2020-04-30 12:27:48 +0200

    extracted common functions out of moinput

--- R/calcFloorspace.R
+++
@@ -1,8 +0,0 @@
-
-calcFloorspace <- function() {
-  
-  data <- readSource("EDGE", subtype = "Floorspace") 
-  
-  
-  return(list(x=data,weight=NULL, unit = "million m2", description = "Buildings floorspace"))
-}
--- R/convertDLR.R
+++
@@ -1,5 +0,0 @@
-convertDLR <- function(x) {
-  # fill all missing countries with 0
-  x <- toolCountryFill(x,fill=0)
-  return(x)
-}
