a4601e40850fdf97dbb2271286f867079dad3e25
FeBeier <felicitas.beier@pik-potsdam.de>
2020-07-17 14:44:53 +0200

    Included flooded irrigation in calcLUH2v2

--- R/calcLUH2v2.R
+++ R/calcLUH2v2.R
@@ -2,13 +2,13 @@
 #' @description Integrates the LUH2v2 landuse-dataset.
 #'
 #' @param landuse_types magpie: magpie landuse classes. LUH2v2: original landuse classes.
-#' @param irrigation if true, the areas are returned sperated by irrigated and rainfed. Irrigation includes flooded area. possible inconsistencies, to be checked.
+#' @param irrigation if true, the areas are returned separated by irrigated and rainfed. Irrigation includes flooded area. possible inconsistencies, to be checked.
 #' @param cellular if true, dataset is returned on 0.5 degree resolution
 #' @param cells Switch between "magpiecell" (59199) and "lpjcell" (67420)
 #' @param selectyears defaults to past
 #' 
 #' @return List of magpie objects with results on country level, weight on country level, unit and description.
-#' @author Benjamin Leon Bodirsky, Florian Humpenoeder, Felicitas Beier
+#' @author Benjamin Leon Bodirsky, Florian Humpenoeder, Jens Heinke, Felicitas Beier
 #' @seealso
 #' \code{\link{calcLanduseInitialisation}}
 #' @examples
@@ -17,10 +17,11 @@
 #' calcOutput("LUH2v2")
 #' }
 #' @importFrom magclass getNames
+#' @importFrom magpiesets findset
 
 calcLUH2v2<-function(landuse_types="magpie",irrigation=FALSE,cellular=FALSE,cells="magpiecell",selectyears="past"){
   
-
+  
   selectyears <- sort(findset(selectyears,noset = "original"))
   
   if (cellular){
@@ -31,9 +32,8 @@ calcLUH2v2<-function(landuse_types="magpie",irrigation=FALSE,cellular=FALSE,cell
       x <- readSource("LUH2v2",subtype = "states",convert="onlycorrect")[,selectyears,]
       getSets(x) <- c("fake","lpjcell","t","landuse")   
     }
-
+    
     if (irrigation){
-      
       if (cells=="lpjcell"){
         y<-readSource("LUH2v2",subtype="irrigation_lpjcell",convert="onlycorrect")[,selectyears,]
       } else if(cells=="magpiecell"){
@@ -48,8 +48,8 @@ calcLUH2v2<-function(landuse_types="magpie",irrigation=FALSE,cellular=FALSE,cell
       y <- readSource("LUH2v2",subtype="irrigation",convert=TRUE)[,selectyears,]
     }
   }
-
-
+  
+  
   if (irrigation){
     if(is.null(selectyears)){vcat(verbosity = 3,"too many years may lead to memory problems if irrigation=T")}
     #### this section can be placed into the correct function once it exists
@@ -58,6 +58,22 @@ calcLUH2v2<-function(landuse_types="magpie",irrigation=FALSE,cellular=FALSE,cell
     #y[,,"irrig_c3ann"]<-y[,,"irrig_c3ann"]+y[,,"flood"]
     #y<-y[,,c("irrig_c3ann","irrig_c3per" ,"irrig_c4ann","irrig_c4per","irrig_c3nfx")]
     ###
+    ## Calculate irrigated area accounting for flooded
+    cropsWOc3ann <- c("c3per","c4ann","c4per","c3nfx")
+    # calculate non-flooded cropland
+    cl_nonflooded <- x[,,"c3ann"]*(1-y[,,"flood"])
+    cl_nonflooded <- cl_nonflooded + dimSums(x[,,cropsWOc3ann],dim=3)
+    # claculate irrigated cropland
+    cl_irrig <- x[,,"c3ann"]*y[,,"irrig_c3ann"]
+    cl_irrig <- cl_irrig+dimSums(y[,,paste0("irrig_",cropsWOc3ann)]*x[,,cropsWOc3ann], dim=3)
+    # fraction of non-flooded cropland under irrigation
+    frac_irrig <- ifelse(cl_nonflooded>0, cl_irrig/cl_nonflooded, 0)
+    frac_irrig <- pmin(frac_irrig, 1) # just to be sure
+    frac_irrig <- collapseNames(frac_irrig)
+    # assign to y
+    y[,,"irrig_c3ann"] <- y[,,"flood"]+(1-y[,,"flood"])*frac_irrig
+    y[,,paste0("irrig_",cropsWOc3ann)] <- frac_irrig
+
     y<-y[,,c("irrig_c3ann","irrig_c3per" ,"irrig_c4ann","irrig_c4per","irrig_c3nfx")]
     getNames(y)<-substring(getNames(y),7)
     x<-add_dimension(x,dim = 3.2,add = "irrigation",nm = "total")
