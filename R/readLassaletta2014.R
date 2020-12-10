739f786a4356eb879f0ecf2994a741e047e2d629
FeBeier <felicitas.beier@pik-potsdam.de>
2020-12-10 21:06:30 +0100

    bug fix part 2 of readLassaletta2014: same as above for second subtype (i.e. problem with country names in row label instead of first row of data frame)

--- R/readLassaletta2014.R
+++ R/readLassaletta2014.R
@@ -20,17 +20,17 @@
 
 readLassaletta2014<-function(subtype="budget"){
   if(subtype=="budget"){
-    out<-NULL
+    out <- NULL
     for(sheet_x in 3:8){
-      data<-as.data.frame(read_excel("BUDGET_N_Countries_1961_2009_Paper_NUE_Lassaletta_etal_ERL_3.xlsx",sheet = sheet_x))
+      data        <- as.data.frame(read_excel("BUDGET_N_Countries_1961_2009_Paper_NUE_Lassaletta_etal_ERL_3.xlsx",sheet = sheet_x))
       names(data) <- gsub(" ",".",names(data))
-      tmp<-strsplit(names(data),split = "\\.")
-      year<-paste0("y",unlist(lapply(tmp,FUN=function(x){x[length(x)]})))
-      year<-year[2:length(year)]
-      indicator<-strsplit(names(data)[2],split = "\\.")[[1]]
-      indicator<-paste(indicator[1:(length(indicator)-1)],collapse="_")
-      countries<-data[,1]
-      countries<-   toolCountry2isocode(countries,mapping=c(
+      tmp  <- strsplit(names(data),split = "\\.")
+      year <- paste0("y",unlist(lapply(tmp,FUN=function(x){x[length(x)]})))
+      year <- year[2:length(year)]
+      indicator <- strsplit(names(data)[2],split = "\\.")[[1]]
+      indicator <- paste(indicator[1:(length(indicator)-1)],collapse="_")
+      countries <- data[,1]
+      countries <- toolCountry2isocode(countries,mapping=c(
           "Belgium-Luxemburg"="BEL",                 
           "Bolivia (Plurinational State of)"  ="BOL",
          # "Cote d'Ivoire" = "CIV",                    
@@ -40,12 +40,12 @@ readLassaletta2014<-function(subtype="budget"){
           "Venezuela (Bolivarian Republic of)"="VEN",
           "Yugoslav SFR" ="YUG"
         ))
-      rownames(data)<-countries
-      data[,1] <- rownames(data)
-      colnames(data)<-c("reg",year)
-      data<-as.magpie(data)
-      getNames(data)<-indicator
-      out<-mbind(out,data)
+      rownames(data) <- countries
+      data[,1]       <- rownames(data)
+      colnames(data) <- c("reg",year)
+      data           <- as.magpie(data)
+      getNames(data) <- indicator
+      out            <- mbind(out,data)
     }
     out<-out*setNames(out[,,"Surfaces_ha"],NULL)
     out<-setNames(out[,,c(
@@ -55,9 +55,9 @@ readLassaletta2014<-function(subtype="budget"){
     ))
     out<-out/10^9
   } else if (subtype=="fert_to_cropland"){
-    a<-read.csv(file="erl502906suppdata1annex.csv",sep = ";",header = 1,row.names = 1)
-    dimnames(a)[[2]]<-gsub(dimnames(a)[[2]],pattern = "X",replacement = "y")
-    dimnames(a)[[1]]<-toolCountry2isocode(dimnames(a)[[1]],mapping=c(
+    a <- read.csv(file="erl502906suppdata1annex.csv",sep = ";",header = 1,row.names = 1)
+    dimnames(a)[[2]] <- gsub(dimnames(a)[[2]],pattern = "X",replacement = "y")
+    dimnames(a)[[1]] <- toolCountry2isocode(dimnames(a)[[1]],mapping=c(
       "Belgium-Luxemburg"="BEL",                 
       "Bolivia (Plurinational State of)"  ="BOL",
       # "Cote d'Ivoire" = "CIV",                    
@@ -68,8 +68,9 @@ readLassaletta2014<-function(subtype="budget"){
       "Yugoslav SFR" ="YUG",
       "dprepublic of korea"="PRK"
     ))
-    out<-as.magpie(a)
+    a   <- cbind(data.frame(region=rownames(a),stringsAsFactors=F), a)
+    out <- as.magpie(a)
   } else {stop("unknown subtype")}
-  out<-clean_magpie(out)
+  out <- clean_magpie(out)
   return(out)
 }
