cdf21ca805bdcfcfa8b50871657a7ea1bab9c7ca
abhimishr <mishra.abhijeet28@gmail.com>
2020-12-24 18:14:42 +0100

    Added absolute growing stock targets

---
+++ R/calcGrowingStockPlantAbsolute.R
@@ -0,0 +1,31 @@
+#' @title calcGrowingStockPlantAbsolute
+#' @description 
+#' Calculates the growing stocks on FAO data.
+#'
+#' @return List of magpie objects with results on country level, weight on country level, unit and description.
+#' @author Abhijeet Mishra
+#' @examples
+#' \dontrun{ 
+#' calcOutput("GrowingStockPlantAbsolute",aggregate=TRUE)
+#' }
+#' 
+#' @importFrom magpiesets FRAnames
+#' @export
+
+calcGrowingStockPlantAbsolute <- function(){
+  
+  ## Read Growing Stock
+  out <- readSource("FRA2020",subtype = "growing_stock",convert = TRUE)
+  x <- out[,,grep(pattern = "tot",x = getNames(out),value = TRUE)]
+  getNames(x) <- gsub(pattern = "gs_tot_",replacement = "",x = getNames(x))
+  getNames(x) <- FRAnames(getNames(x))
+  
+  x <- setYears(collapseNames(x[,"y2000","Managed Forest"]),NULL)
+  
+  return(list(x=x,
+              weight=NULL,
+              min=0,
+              unit="Mm3",
+              description="Calculates Growing stocks as reported by Forest Resources Assessment Data 2020."))
+  
+}
