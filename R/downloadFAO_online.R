44307f1500a6045273c0426995788a2c47b02e41
David Klein <dklein@pik-potsdam.de>
2021-01-04 13:10:09 +0100

    Add meta data to subtypes

--- R/downloadFAO_online.R
+++ R/downloadFAO_online.R
@@ -2,70 +2,109 @@
 
 downloadFAO_online <- function(subtype) {
   
-  links <- c(CBCrop="CommodityBalances_Crops_E_All_Data.zip",
-             CBLive="CommodityBalances_LivestockFish_E_All_Data.zip",
-             Crop="Production_Crops_E_All_Data.zip",
-             CropProc="Production_CropsProcessed_E_All_Data.zip",
-             #Fbs="FoodBalanceSheets_E_All_Data.zip", #should not be used, use CB and FS or calcFAOharmonized() instead
-             Fbs="FoodBalanceSheets_E_All_Data.zip", 
-             Fertilizer="Environment_Fertilizers_E_All_Data.zip",
-             #Fodder="Fodder.csv",
-             FoodSecurity="Food_Security_Data_E_All_Data.zip",
-             FSCrop="FoodSupply_Crops_E_All_Data.zip",
-             FSLive="FoodSupply_LivestockFish_E_All_Data.zip",
-             #Land="Resources_Land_E_All_Data.zip",
-             LiveHead="Production_Livestock_E_All_Data.zip",
-             LivePrim="Production_LivestockPrimary_E_All_Data.zip",
-             LiveProc="Production_LivestockProcessed_E_All_Data.zip",
-             Pop="Population_E_All_Data.zip",
-             PricesProducerAnnual="Prices_E_All_Data.zip",
-             PricesProducerAnnualLCU="Prices_E_All_Data.zip",
-
-             EmisAgTotal="Emissions_Agriculture_Agriculture_total_E_All_Data.zip",
-             EmisAgBurnCropResid="Emissions_Agriculture_Burning_crop_residues_E_All_Data.zip",
-             EmisAgBurnSavanna="Emissions_Agriculture_Burning_Savanna_E_All_Data.zip",
-             EmisAgCropResid="Emissions_Agriculture_Crop_Residues_E_All_Data.zip",
-             EmisAgCultOrgSoil="Emissions_Agriculture_Cultivated_Organic_Soils_E_All_Data.zip",
-             EmisAgEnergy="Emissions_Agriculture_Energy_E_All_Data.zip",
-             EmisAgEntericFerment="Emissions_Agriculture_Enteric_Fermentation_E_All_Data.zip",
-             EmisAgManureSoil="Emissions_Agriculture_Manure_applied_to_soils_E_All_Data.zip", 
-             EmisAgManurePasture="Emissions_Agriculture_Manure_left_on_pasture_E_All_Data.zip",
-             EmisAgManureManag="Emissions_Agriculture_Manure_Management_E_All_Data.zip",
-             EmisAgRiceCult="Emissions_Agriculture_Rice_Cultivation_E_All_Data.zip",
-             EmisAgSynthFerti="Emissions_Agriculture_Synthetic_Fertilizers_E_All_Data.zip",
-             
-             EmisLuBurnBiomass="Emissions_Land_Use_Burning_Biomass_E_All_Data.zip",
-             EmisLuCrop="Emissions_Land_Use_Cropland_E_All_Data.zip",
-             EmisLuForest="Emissions_Land_Use_Forest_Land_E_All_Data.zip",
-             EmisLuGrass="Emissions_Land_Use_Grassland_E_All_Data.zip",
-             EmisLuTotal="Emissions_Land_Use_Land_Use_Total_E_All_Data.zip",
-             
-             ValueOfProd="Value_of_Production_E_All_Data.zip",
-             ForestProdTrade="Forestry_E_All_Data_(Normalized).zip")
+  settings <- list(CBCrop            = list(title = "Food Balance: Commodity Balances - Crops Primary Equivalent.",
+                                              url = "CommodityBalances_Crops_E_All_Data.zip"),
+                CBLive               = list(title = "Food Balance: Commodity Balances - Livestock and Fish Primary Equivalent",
+                                              url = "CommodityBalances_LivestockFish_E_All_Data.zip"),
+                Crop                 = list(title = "",
+                                              url = "Production_Crops_E_All_Data.zip"),
+                CropProc             = list(title = "",
+                                              url = "Production_CropsProcessed_E_All_Data.zip"),
+                #Fbs                 = list(title = "", #should not be used, use CB and FS or calcFAOharmonized() instead
+                #                             url = "FoodBalanceSheets_E_All_Data.zip"), 
+                Fbs                  = list(title = "",
+                                              url = "FoodBalanceSheets_E_All_Data.zip"), 
+                Fertilizer           = list(title = "",
+                                              url = "Environment_Fertilizers_E_All_Data.zip"),
+                #Fodder              = list(title = "",
+                #                             url = "Fodder.csv",
+                FoodSecurity         = list(title = "",
+                                              url = "Food_Security_Data_E_All_Data.zip"),
+                FSCrop               = list(title = "",
+                                              url = "FoodSupply_Crops_E_All_Data.zip"),
+                FSLive               = list(title = "",
+                                              url = "FoodSupply_LivestockFish_E_All_Data.zip"),
+                Land                 = list(title = "",
+                                              url = "Inputs_LandUse_E_All_Data_(Normalized).zip"), # old source file: Resources_Land_E_All_Data.zip
+                LiveHead             = list(title = "",
+                                              url = "Production_Livestock_E_All_Data.zip"),
+                LivePrim             = list(title = "",
+                                              url = "Production_LivestockPrimary_E_All_Data.zip"),
+                LiveProc             = list(title = "",
+                                              url = "Production_LivestockProcessed_E_All_Data.zip"),
+                Pop                  = list(title = "",
+                                              url = "Population_E_All_Data.zip"),
+                PricesProducerAnnual = list(title = "",
+                                              url = "Prices_E_All_Data.zip"),
+                PricesProducerAnnualLCU = list(title = "",
+                                              url = "Prices_E_All_Data.zip"),
+                
+                EmisAgTotal          = list(title = "",
+                                              url = "Emissions_Agriculture_Agriculture_total_E_All_Data.zip"),
+                EmisAgBurnCropResid  = list(title = "",
+                                              url = "Emissions_Agriculture_Burning_crop_residues_E_All_Data.zip"),
+                EmisAgBurnSavanna    = list(title = "",
+                                              url = "Emissions_Agriculture_Burning_Savanna_E_All_Data.zip"),
+                EmisAgCropResid      = list(title = "",
+                                              url = "Emissions_Agriculture_Crop_Residues_E_All_Data.zip"),
+                EmisAgCultOrgSoil    = list(title = "",
+                                              url = "Emissions_Agriculture_Cultivated_Organic_Soils_E_All_Data.zip"),
+                EmisAgEnergy         = list(title = "",
+                                              url = "Emissions_Agriculture_Energy_E_All_Data.zip"),
+                EmisAgEntericFerment = list(title = "",
+                                              url = "Emissions_Agriculture_Enteric_Fermentation_E_All_Data.zip"),
+                EmisAgManureSoil     = list(title = "",
+                                              url = "Emissions_Agriculture_Manure_applied_to_soils_E_All_Data.zip"), 
+                EmisAgManurePasture  = list(title = "",
+                                              url = "Emissions_Agriculture_Manure_left_on_pasture_E_All_Data.zip"),
+                EmisAgManureManag    = list(title = "",
+                                              url = "Emissions_Agriculture_Manure_Management_E_All_Data.zip"),
+                EmisAgRiceCult       = list(title = "",
+                                              url = "Emissions_Agriculture_Rice_Cultivation_E_All_Data.zip"),
+                EmisAgSynthFerti     = list(title = "",
+                                              url = "Emissions_Agriculture_Synthetic_Fertilizers_E_All_Data.zip"),
+                
+                EmisLuBurnBiomass    = list(title = "",
+                                              url = "Emissions_Land_Use_Burning_Biomass_E_All_Data.zip"),
+                EmisLuCrop           = list(title = "",
+                                              url = "Emissions_Land_Use_Cropland_E_All_Data.zip"),
+                EmisLuForest         = list(title = "",
+                                              url = "Emissions_Land_Use_Forest_Land_E_All_Data.zip"),
+                EmisLuGrass          = list(title = "",
+                                              url = "Emissions_Land_Use_Grassland_E_All_Data.zip"),
+                EmisLuTotal          = list(title = "",
+                                              url = "Emissions_Land_Use_Land_Use_Total_E_All_Data.zip"),
+                
+                ValueOfProd     = list(title = "",
+                                         url = "Value_of_Production_E_All_Data.zip"),
+                ForestProdTrade = list(title = "",
+                                        url = "Forestry_E_All_Data_(Normalized).zip")
+  )
   
-  settings <- list( CBCrop= list(title = "Food Balance: Commodity Balances - Crops Primary Equivalent.",
-                                   url = "CommodityBalances_Crops_E_All_Data.zip"),
-                    CBLive= list(title = "Food Balance: Commodity Balances - Livestock and Fish Primary Equivalent",
-                                   url = "CommodityBalances_LivestockFish_E_All_Data.zip")
-                    )
   
   meta <- toolSubtypeSelect(subtype,settings)
   
-  # add base link (preserve 'links' as named vector by using '[] <-')
-  links[] <- paste0("http://fenixservices.fao.org/faostat/static/bulkdownloads/",links)
+  # xml file describing the FAO data of the bulk download
+  # download.file(url = "http://fenixservices.fao.org/faostat/static/bulkdownloads/datasets_E.xml", destfile = "FAO_datasets_E.xml")
+  # download.file(url = "http://fenixservices.fao.org/faostat/static/bulkdownloads/datasets_E.json", destfile = "FAO_datasets_E.json")
+  # sort(xml_text(xml_find_all(x, "//FileLocation")))
+  # http://www.fao.org/es/faodef/fdef11e.htm
+  # http://www.fao.org/3/ca7570en/ca7570en.pdf
+  # http://fenixservices.fao.org/faostat/static/releasecalendar/Default.aspx
   
-  fnames <- sapply(links, function(x){tail(strsplit(x, split = "/")[[1]], 1)})
   
-  ### execute downloading
-  if (is.null(subtype)) {
-    lapply(1:length(links), FUN = function(x){ download.file(links[x], destfile=fnames[x], mode="wb")})
-  } else {
-    download.file(links[[subtype]], destfile=fnames[[subtype]], mode="wb")
-  }
+  download.file(paste0("http://fenixservices.fao.org/faostat/static/bulkdownloads/",meta$url), destfile=meta$url, mode="wb")
 
-  ###  unzip files
-  #zipfiles <- list.files(pattern=".zip$")
-  #lapply(zipfiles, unzip)
-  #lapply(zipfiles, unlink)
+  # Compose meta data by adding elements that are the same for all subtypes.
+  return(list(url           = meta$url,
+              doi           = "not available",
+              title         = meta$title,
+              author        = "FAOSTAT",
+              version       = "not available",
+              release_date  = "2020-12-03",
+              license       = "Creative Commons Attribution-NonCommercial-ShareAlike 3.0 IGO (CC BY-NC- SA 3.0 IGO)",
+              reference     = "not available")
+  )
+  
 
 }
