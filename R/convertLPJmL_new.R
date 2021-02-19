fcef84a790f7b58bdee1b28dddec17bf26fd7ae0
Kristine Karstens <karstens@pik-potsdam.de>
2021-02-19 03:31:39 +0100

    add convert function for LPJmL_new

---
+++ R/convertLPJmL_new.R
@@ -0,0 +1,18 @@
+#' @title convertLPJmL_new
+#' @description Convert LPJmL content (dummy function)
+#' @param x magpie object provided by the read function
+#' 
+#' @author Kristine Karstens
+#' @seealso
+#' \code{\link{readLPJmL}}
+#' @examples
+#'
+#' \dontrun{
+#'   readSource("LPJmL", convert=TRUE)
+#' }
+#'
+
+convertLPJmL_new <- function(x){
+
+  return(x)
+}
