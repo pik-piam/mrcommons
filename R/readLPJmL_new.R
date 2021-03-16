887cdfff992151451974b5b5a959b5de9eaa870b
Marcos Alves <mppalves@gmail.com>
2021-03-16 13:13:02 +0100

    modified read and downloadLPJmL_new

--- R/readLPJmL_new.R
+++ R/readLPJmL_new.R
@@ -1,8 +1,8 @@
 #' @title readLPJmL_new
 #' @description Read LPJmL content
-#' @param subtype Switch between different input
+#' @param subtype Switch between different input (eg. "LPJmL5.2_Pasture:IPSL_CM6A_LR:ssp126_co2_limN_00:soilc_past_hist")
 #' @return List of magpie objects with results on cellular level, weight, unit and description.
-#' @author Kristine Karstens, Abhijeet Mishra, Felicitas Beier
+#' @author Kristine Karstens, Abhijeet Mishra, Felicitas Beier, Marcos Alves
 #' @seealso
 #' \code{\link{readLPJ}}
 #' @examples
@@ -17,10 +17,10 @@
 #' @importFrom stringr str_subset str_trim str_split
 
 readLPJmL_new <- function(subtype="LPJmL4_for_MAgPIE_84a69edd:GSWP3-W5E5:historical:soilc"){
-
+  
   subtype     <- toolSplitSubtype(subtype, list(version=NULL, climatemodel=NULL, scenario=NULL, variable=NULL))$variable
   
-   .prepareLPJ <- function(datatype = numeric(),
+  .prepareLPJ <- function(datatype = numeric(),
                           bytes    = 4,
                           monthly  = FALSE,
                           nbands   = NULL) { # nbands will be overwritten for clm data
@@ -43,13 +43,25 @@ readLPJmL_new <- function(subtype="LPJmL4_for_MAgPIE_84a69edd:GSWP3-W5E5:histori
     } else if(file_type=="bin"){
       
       out        <- readLines("lpjml_log.out")
-      start_year <- as.numeric(str_trim(unlist(str_split(str_subset(out,'Output written in year:'),":")))[2])
-      end_year   <- as.numeric(str_trim(unlist(str_split(str_subset(out,'Last year:'),":")))[2])
+      start_year <- out %>% 
+        str_subset('Output written in year:') %>% 
+        str_split(":") %>% 
+        unlist() %>% 
+        str_trim() %>% 
+        subset(c(F,T)) %>%
+        as.numeric()
+      end_year   <- out %>% 
+        str_subset('Last year:') %>% 
+        str_split(":") %>% 
+        unlist() %>% 
+        str_trim() %>% 
+        subset(c(F,T)) %>%
+        as.numeric()
       years      <- seq(start_year,end_year,1)
       headlines  <- 0
       
     } else {stop("File format of LPJmL input data unknown. Please provide .clm or .bin file format.")}
-
+    
     
     x <- readLPJ(
       file_name       = file_name,
@@ -69,32 +81,33 @@ readLPJmL_new <- function(subtype="LPJmL4_for_MAgPIE_84a69edd:GSWP3-W5E5:histori
     x        <- collapseNames(as.magpie(x, spatial=1))
     x        <- collapseDim(addLocation(x), dim="N")
     x        <- clean_magpie(x)
-  
+    
     return(x)
   }
   
   if(subtype%in%c("soilc","litc", "vegc", "alitfallc","alitterfallc", "aet",
-                  "vegc_grass", "litc_grass", "soilc_grass", "aprec")){
-
+                  "vegc_grass", "litc_grass", "soilc_grass", "aprec", "soilc_past_hist", "soilc_past_scen")){
+    
     x <- .prepareLPJ(nbands = 1)
-   
+    
   } else if(grepl("*date*", subtype)){
-
+    
     x <- .prepareLPJ(nbands = 24, datatype = integer(), bytes = 2)
-   
+    
   } else if(subtype%in%c("soilc_layer")){
-
+    
     x <- .prepareLPJ(nbands = 5)
-
+    
   } else if(grepl("mdischarge|mrunoff|mpet", subtype)){
     
     x <- .prepareLPJ(monthly = TRUE)
-
+    
   } else if(grepl("harvest|irrig|cwater_b", subtype)){
-
+    
     x <- .prepareLPJ(nbands = 32)
     
   } else {stop(paste0("subtype ",subtype," is not existing"))}
-
+  
   return(x)
 }
+
