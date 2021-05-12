764326497caaf87c1c3e146de394cdadf69d67e3
Kristine Karstens <karstens@pik-potsdam.de>
2021-05-13 00:48:24 +0200

    update LPJml input data, name of yield function

--- R/calcLanduseIntensity.R
+++ R/calcLanduseIntensity.R
@@ -35,7 +35,7 @@ calcLanduseIntensity <- function(sectoral="kcr", rescale = TRUE) {
     CountryToCell <- toolMappingFile(type="cell",name="CountryToCellMapping.csv",readcsv = TRUE)
     
     #Load LPJ yields and area on cell level
-    LPJYields      <- collapseNames(calcOutput("LPJmL_new", version="ggcmi_phase3_nchecks_fbed5c8b_irrig-is-rainf", climatetype="GSWP3-W5E5:historical",
+    LPJYields      <- collapseNames(calcOutput("LPJmL_new", version="ggcmi_phase3_nchecks_fbed5c8b_newparam", climatetype="GSWP3-W5E5:historical",
                                                subtype="harvest", stage="smoothed", aggregate=FALSE)[,selectyears,LPJCroptypes])
     LPJYields      <- toolCoord2Isocell(LPJYields)
     if(sectoral=="kcr") LPJYields  <- toolAggregate(LPJYields, rel=MAGtoLPJ, from="LPJmL", to="MAgPIE", dim=3.1)
@@ -88,7 +88,7 @@ calcLanduseIntensity <- function(sectoral="kcr", rescale = TRUE) {
     CountryToCell <- toolMappingFile(type="cell",name="CountryToCellMapping.csv",readcsv = TRUE)
     
     #Load LPJ yields and area on cell level
-    LPJYields           <- toolCoord2Isocell(calcOutput("LPJmL_new", version="ggcmi_phase3_nchecks_fbed5c8b_irrig-is-rainf", climatetype="GSWP3-W5E5:historical", 
+    LPJYields           <- toolCoord2Isocell(calcOutput("LPJmL_new", version="ggcmi_phase3_nchecks_fbed5c8b_newparam", climatetype="GSWP3-W5E5:historical", 
                                       subtype="harvest", stage="smoothed", aggregate=FALSE, years=selectyears))[,,"mgrass.rainfed"]
     MAGPasturearea      <- calcOutput("LanduseInitialisation", cellular = TRUE, aggregate = FALSE)[,,"past"]
     getNames(LPJYields) <- getNames(MAGPasturearea) <- "pasture"
