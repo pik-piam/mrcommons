b60763d849a05f70dbf45722e70051cb054e415c
mscrawford <mscrawfor@gmail.com>
2021-05-20 16:06:23 +0200

    Bugfix GFED file permissions

--- R/downloadGFED.R
+++ R/downloadGFED.R
@@ -4,34 +4,40 @@
 #'
 #' @author Michael S. Crawford
 #' @seealso \code{\link{downloadSource}} \code{\link{readGFED}}
-#' @examples \dontrun{ a <- downloadSource() }
+#' @examples \dontrun{
+#' a <- downloadSource()
+#' }
 #'
 #' @importFrom purrr walk
 #' @importFrom utils download.file
 
 downloadGFED <- function() {
 
-	# yearly GFED data
-	base_URL <- "https://www.geo.vu.nl/~gwerf/GFED/GFED4/"
-	years <- seq(1997, 2016) # ignoring current years in beta
-	walk(.x = years,
-		 .f = ~
-		 	{
-		 		year_URL <- paste0("GFED4.1s_", .x, ".hdf5")
-		 		download.file(paste0(base_URL, year_URL), destfile = year_URL)
-		 	})
+  # yearly GFED data
+  base_URL <- "https://www.geo.vu.nl/~gwerf/GFED/GFED4/"
+  years <- seq(1997, 2016) # ignoring current years in beta
+  walk(
+    .x = years,
+    .f = ~ {
+      year_URL <- paste0("GFED4.1s_", .x, ".hdf5")
+      download.file(paste0(base_URL, year_URL), destfile = year_URL)
+      Sys.chmod(year_URL, "664", use_umask = FALSE)
+    }
+  )
 
-	# emission factors
-	emissionFactors_URL <- "ancill/GFED4_Emission_Factors.txt"
-	download.file(paste0(base_URL, emissionFactors_URL), destfile = "GFED_emissionsFactors.txt")
+  # emission factors
+  emissionFactors_URL <- "ancill/GFED4_Emission_Factors.txt"
+  download.file(paste0(base_URL, emissionFactors_URL), destfile = "GFED_emissionsFactors.txt")
 
-	return(list(url          = base_URL,
-				title        = "Global Fire Emissions Database",
-				description  = "C and DM emissions from the burning of plant biomass globally, with emission factors.",
-				revision     = "4.1",
-				comment      = "Readme documentation can be found here: https://www.geo.vu.nl/~gwerf/GFED/GFED4/Readme.pdf",
-				unit         = "Spatial data is reported in kg X m^-2 (0.25x0.25 degree grid cells), emission factors are reported in g X per kg.",
-				release_date = "2015-07-03",
-				license      = "Creative Commons Attribution 3.0 License",
-				author       = "Global Fire Emissions Database"))
+  return(list(
+    url          = base_URL,
+    title        = "Global Fire Emissions Database",
+    description  = "C and DM emissions from the burning of plant biomass globally, with emission factors.",
+    revision     = "4.1",
+    comment      = "Readme documentation can be found here: https://www.geo.vu.nl/~gwerf/GFED/GFED4/Readme.pdf",
+    unit         = "Spatial data is reported in kg X m^-2 (0.25x0.25 degree grid cells), emission factors are reported in g X per kg.",
+    release_date = "2015-07-03",
+    license      = "Creative Commons Attribution 3.0 License",
+    author       = "Global Fire Emissions Database"
+  ))
 }
