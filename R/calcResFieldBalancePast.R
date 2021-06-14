567b18e0a7b489219569bd9a4e2bb7c0c73cf256
David Chen <davidch@lt916.pik-potsdam.de>
2021-06-14 16:55:44 +0200

    update ResFieldBalancePast for yearly FAO_online data

--- R/calcResFieldBalancePast.R
+++ R/calcResFieldBalancePast.R
@@ -27,6 +27,13 @@ calcResFieldBalancePast <- function(cellular = FALSE, products = "sum", scenario
     
     burnshr           <- calcOutput("ResCombustEff",aggregate = FALSE)[,,getNames(production,dim=1)]
     dev_state_past    <- collapseNames(calcOutput("DevelopmentState",aggregate = F)[,past,"SSP2"])
+    
+    #interpolate devstate years 
+    iyears <- getYears(production)
+    dev_state_past <- time_interpolate(dev_state_past, interpolated_year = iyears, integrate_interpolated_years = TRUE)
+    dev_state_past[,c(1961:1964),] <- setYears(dev_state_past[,1965,],NULL)
+    dev_state_past[,c(2011:getYears(dev_state_past, as.integer = TRUE)[length(getYears(dev_state_past))]),] <- setYears(dev_state_past[,2010,],NULL)
+    
     if(cellular){
       dev_state_past    <- toolIso2CellCountries(dev_state_past)
     }
