7e8a81be78078f70d728b94dd53d452862133a31
emolinab <mbacca@pik-potsdam.de>
2021-07-02 11:42:09 +0200

    units correction on factor requirements related parameters

--- R/calcVoP_AFF.R
+++ R/calcVoP_AFF.R
@@ -1,6 +1,7 @@
-#' @title calcVoP_AFF 
-#' @description Calculates the overall value of production of the agriculture, forestry and fisheries sectors. Forestry and Fisheries are calculated from exports values.
-#' 
+#' @title calcVoP_AFF
+#' @description Calculates the overall value of production of the agriculture, 
+#' forestry and fisheries sectors. Forestry and Fisheries are calculated from exports values.
+#'
 #'
 #'
 #' @return magpie object. in mio. current USD units
@@ -9,77 +10,85 @@
 #' @importFrom magclass dimSums
 #' @seealso \code{\link{calcOutput}}
 #' @examples
-#'
 #' \dontrun{
 #' a <- calcOutput("VoP_AFF")
-
 #' }
 #'
 calcVoP_AFF <- function() {
 
+#### GDP
+  GDP <- calcOutput("GDPppp", aggregate = FALSE)[, c(2005, 2020, 2015), "gdp_SSP2"]
+  GDP_con <- setNames(setYears((GDP[, 2020, ] / GDP[, 2015, ]), NULL), NULL)
+
 #### Value of production for Agriculture (crops and livestock)
-  Ag<-c("2041|Crops.Gross_Production_Value_(constant_2014_2016_thousand_I$)_(1000_Int_$)",
+  Ag <- c("2041|Crops.Gross_Production_Value_(constant_2014_2016_thousand_I$)_(1000_Int_$)",
         "2044|Livestock.Gross_Production_Value_(constant_2014_2016_thousand_I$)_(1000_Int_$)")
- 
-   #factor converts from mio. 05 USD to current USD
-  VoP_agriculture<-dimSums(readSource("FAO_online","ValueOfProd")[,,Ag]/1000,dim=3)*(1+0.04)^5 
-  getNames(VoP_agriculture)<-"Agriculture"
-  
+
+   # factor converts from mio. 05 USD to current USD
+  VoP_agriculture <- dimSums(readSource("FAO_online", "ValueOfProd")[, , Ag] / 1000, dim = 3) * GDP_con
+  getNames(VoP_agriculture) <- "Agriculture"
+
 
 #### Value of production fisheries
-  
-  #export value and quantity of fish and other aquatic products
-  export_fish_value <- readSource("FishstatJ_FAO",subtype="exportsValue") # 1000 current USD
-  export_fish_tonNet <- readSource("FishstatJ_FAO",subtype="exportsQuantity")#ton_net 
-  
-  Fish_Cat<-c("2961|Aquatic Products, Other + (Total).production",
+
+  # export value and quantity of fish and other aquatic products
+  export_fish_value <- readSource("FishstatJ_FAO", subtype = "exportsValue") # 1000 current USD
+  export_fish_tonNet <- readSource("FishstatJ_FAO", subtype = "exportsQuantity") # ton_net
+
+  Fish_Cat <- c("2961|Aquatic Products, Other + (Total).production",
               "2960|Fish, Seafood + (Total).production")
-  
-  production_fish_tonNet <- dimSums(readSource("FAO","CBLive")[,,Fish_Cat],dim=3.1)#ton net
-  
-  #common years
-  years_fish<-intersect(intersect(getYears(export_fish_value),getYears((export_fish_tonNet))),getYears(production_fish_tonNet))
-  cells_fish<-intersect(intersect(getCells(export_fish_value),getCells((export_fish_tonNet))),getCells(production_fish_tonNet))
-  
-  #Value of production for fish and aquatic products -> Production*export_price
-  VoP_fish<-export_fish_value[cells_fish,years_fish,]/export_fish_tonNet[cells_fish,years_fish,]*production_fish_tonNet[cells_fish,years_fish,]/1000 # mio. current USD
-  VoP_fish[!is.finite(VoP_fish)]<-0
-  getNames(VoP_fish)<-"Fisheries"
+
+  production_fish_tonNet <- dimSums(readSource("FAO", "CBLive")[, , Fish_Cat], dim = 3.1) # ton net
+
+  # common years
+  years_fish <- intersect(intersect(getYears(export_fish_value), 
+                                    getYears((export_fish_tonNet))), getYears(production_fish_tonNet))
+  cells_fish <- intersect(intersect(getCells(export_fish_value), 
+                                    getCells((export_fish_tonNet))), getCells(production_fish_tonNet))
+
+  # Value of production for fish and aquatic products -> Production*export_price
+  VoP_fish <- export_fish_value[cells_fish, years_fish, ] / export_fish_tonNet[cells_fish, years_fish, ] * production_fish_tonNet[cells_fish, years_fish, ] / 1000 # mio. current USD
+  VoP_fish[!is.finite(VoP_fish)] <- 0
+  getNames(VoP_fish) <- "Fisheries"
 
 
 #### Value of production forestry
-  
-  Forest_cat<-c("Roundwood.Export_Value_(Mio_US$)",
+
+  Forest_cat <- c("Roundwood.Export_Value_(Mio_US$)",
                 "Roundwood.Export_Quantity_(m3)",
                 "Roundwood.Production_(m3)")
 
-  VoP_forestry_data <- readSource("FAO","ForestProdTrade")[,,Forest_cat]
-  
-  price_forestry<-VoP_forestry_data[,,"Roundwood.Export_Value_(Mio_US$)"]/VoP_forestry_data[,,"Roundwood.Export_Quantity_(m3)"]
-  
-  #to convert from each year money value to 2020
-  for (i in getYears(price_forestry,as.integer = TRUE)){
-    price_forestry[,i,] <- price_forestry[,i,]*(1+0.04)^(2020-i) 
-  }
-  
-  price_forestry[!is.finite(price_forestry)]<-0
+  VoP_forestry_data <- readSource("FAO", "ForestProdTrade")[, , Forest_cat]
 
-  VoP_forestry<-toolCountryFill(x = VoP_forestry_data[,,"Roundwood.Production_(m3)"]*price_forestry,fill = 0) #mio. current USD
-  getNames(VoP_forestry)<-"Forestry"
+  price_forestry <- VoP_forestry_data[, , "Roundwood.Export_Value_(Mio_US$)"] / VoP_forestry_data[, , "Roundwood.Export_Quantity_(m3)"]
+
+  # to convert from each year money value to 2020
+  GDP <- calcOutput("GDPppp", aggregate = FALSE)[, , "gdp_SSP2"]
+  years <- getYears(price_forestry)
+  years <- intersect(getYears(GDP), years)
+
+  GDP_con <- setYears(GDP[, 2020, ] / GDP[, years, ], years)
+
+  price_forestry <- price_forestry[, years, ] * GDP_con
+  price_forestry[!is.finite(price_forestry)] <- 0
+  
+  # mio. current USD
+  VoP_forestry <- toolCountryFill(x = VoP_forestry_data[, , "Roundwood.Production_(m3)"] * price_forestry, fill = 0)
+  getNames(VoP_forestry) <- "Forestry"
 
 ################
-  
-  #magpie object to return
-  years_VoP<-intersect(intersect(getYears(VoP_agriculture),getYears((VoP_fish))),getYears(VoP_forestry))
-  cells_VoP<-intersect(intersect(getCells(VoP_agriculture),getCells((VoP_fish))),getCells(VoP_forestry))
 
-  x<-mbind(VoP_agriculture[cells_VoP,years_VoP,],VoP_fish[cells_VoP,years_VoP,],VoP_forestry[cells_VoP,years_VoP,])
-  
-  
-  return(list(x=x,
-         weight=NULL,
-         mixed_aggregation=NULL,
-         unit="mio. current USD units",
-         description=" Value of production for the agriculture, forestry and fisheries sector"))
+  # magpie object to return
+  years_VoP <- intersect(intersect(getYears(VoP_agriculture), getYears((VoP_fish))), getYears(VoP_forestry))
+  cells_VoP <- intersect(intersect(getCells(VoP_agriculture), getCells((VoP_fish))), getCells(VoP_forestry))
+
+  x <- mbind(VoP_agriculture[cells_VoP, years_VoP, ], VoP_fish[cells_VoP, years_VoP, ], 
+             VoP_forestry[cells_VoP, years_VoP, ])
+
+
+  return(list(x = x,
+         weight = NULL,
+         mixed_aggregation = NULL,
+         unit = "mio. current USD units",
+         description = " Value of production for the agriculture, forestry and fisheries sector"))
 }
-
--- R/calcVoP_crops.R
+++ R/calcVoP_crops.R
@@ -1,10 +1,12 @@
 #' @title calcVoP_crops
-#' @description Calculates the value of production of individual production items or its fraction compared to overall Value of Production (Agriculture,Fish,Forestry).
-#' 
+#' @description Calculates the value of production of individual production items or 
+#' its fraction compared to overall Value of Production (Agriculture,Fish,Forestry).
 #'
-#' 
 #'
-#' @param output defines if output should be given as an "absolute" value or as a "fraction" of the overall value of production.
+#'
+#'
+#' @param output defines if output should be given as an "absolute" value or 
+#' as a "fraction" of the overall value of production.
 #' @param units if output units should be in mio. "USD05", mio. of "USD15", "current" or NULL for output "fraction" type
 #' @return magpie object. in current mio. USD units, in mio. USD05, current USD or NULL (fraction).
 #' @author Edna J. Molina Bacca
@@ -14,66 +16,73 @@
 #'
 #' @seealso \code{\link{calcOutput}}
 #' @examples
-#'
 #' \dontrun{
 #' a <- calcOutput("VoP_crops")
-
 #' }
 #'
-calcVoP_crops <- function(output="absolute",units="USD05") {
+calcVoP_crops <- function(output = "absolute", units = "USD05") {
+
+  #### GDP
+  GDP <- calcOutput("GDPppp", aggregate = FALSE)[, c(2020, 2015, 2005), "gdp_SSP2"]
+  GDP_con <- setNames(setYears((GDP[, 2020, ] / GDP[, 2015, ]), NULL), NULL)
 
   # Value of production for Agriculture, forestry and fishes
-  VoP_AFF<-calcOutput("VoP_AFF",aggregate=FALSE) 
-  VoP_Total<-dimSums(VoP_AFF,dim = 3) #mio. current USD
+  VoP_AFF <- calcOutput("VoP_AFF", aggregate = FALSE)
+  VoP_Total <- dimSums(VoP_AFF, dim = 3) # mio. current USD
 
   # Value of production of indiviual items
-  VoP_All<-readSource("FAO_online","ValueOfProd")[,,"Gross_Production_Value_(constant_2014_2016_thousand_I$)_(1000_Int_$)"]/1000*(1+0.04)^5
-  getNames(VoP_All)<-gsub("\\..*","",getNames(VoP_All))
+  VoP_All <- readSource("FAO_online", "ValueOfProd")[, ,"Gross_Production_Value_(constant_2014_2016_thousand_I$)_(1000_Int_$)"] / 1000 * GDP_con
+  getNames(VoP_All) <- gsub("\\..*", "", getNames(VoP_All))
   getNames(VoP_All)[getNames(VoP_All) == "257|Oil, palm"] <- "257|Oil palm"
 
-  #items for aggregation
-  mappingFAO<-toolGetMapping("FAO_VoP_kcr.csv",type="sectoral")
-  items_intersect<-intersect(getNames(VoP_All),unique(mappingFAO$ProductionItem))
-  mappingFAO<-mappingFAO[mappingFAO$ProductionItem %in% items_intersect,]
-  
-  #Aggregation to magpie objects
-  VoP_kcr_aggregated<-toolAggregate(VoP_All[,,items_intersect],rel=mappingFAO,from="ProductionItem",to="k",weight=NULL,dim=3)
+  # items for aggregation
+  mappingFAO <- toolGetMapping("FAO_VoP_kcr.csv", type = "sectoral")
+  items_intersect <- intersect(getNames(VoP_All), unique(mappingFAO$ProductionItem))
+  mappingFAO <- mappingFAO[mappingFAO$ProductionItem %in% items_intersect, ]
 
-  years<-intersect(getYears(VoP_kcr_aggregated),getYears(VoP_Total))
-  
+  # Aggregation to magpie objects
+  VoP_kcr_aggregated <- toolAggregate(VoP_All[, , items_intersect], rel = mappingFAO, from = "ProductionItem",
+                                      to = "k", weight = NULL, dim = 3)
+
+  years <- intersect(getYears(VoP_kcr_aggregated), getYears(VoP_Total))
+
   # if desired output is fraction over overall value of production (Agriculture, forestry, fishery) or absolute value
-  x<-if(output=="fraction") VoP_kcr_aggregated[,years,]/VoP_Total[,years,] else VoP_kcr_aggregated
+  x <- if (output == "fraction") VoP_kcr_aggregated[, years, ] / VoP_Total[, years, ] else VoP_kcr_aggregated
 
-  x[!is.finite(x)]<-0
-  
-   
-  if (units=="USD05" & output == "absolute") {# Conversions assuming a 4% interest rate
-    x<-x/(1+0.04)^15
-  }else if (units =="USD15" & output == "absolute") {
-     x<- x/(1+0.04)^5
-     } 
-  
-  
-  if (output == "absolute"){
+  x[!is.finite(x)] <- 0
+
+
+  if (units == "USD05" & output == "absolute") {
+    GDP_con <- setNames(setYears((GDP[, 2005, ] / GDP[, 2020, ]), NULL), NULL)
+  } else if (units == "USD15" & output == "absolute") {
+    GDP_con <- setNames(setYears((GDP[, 2015, ] / GDP[, 2020, ]), NULL), NULL)
+  } else if (units == "current" | units == NULL) {
+    GDP_con <- 1
+  } else {
+       stop("Not a valid unit")
+     }
+
+  x <- x * GDP_con
+
+  if (output == "absolute") {
     weight <- NULL
-  }else if (output == "fraction"){
-    Production <- collapseNames(calcOutput("Production",aggregate = FALSE,products="kcr",attributes="dm"))
-    years<-intersect(getYears(Production),getYears(x))
-    names<- intersect(getNames(Production),getNames(x))  
-    
-    weight <- Production[,years,names]
-    x<-x[,years,names]
+  } else if (output == "fraction") {
+    Production <- collapseNames(calcOutput("Production", aggregate = FALSE, products = "kcr", attributes = "dm"))
+    years <- intersect(getYears(Production), getYears(x))
+    names <- intersect(getNames(Production), getNames(x))
+
+    weight <- Production[, years, names]
+    x <- x[, years, names]
     weight[x == 0] <- 0
-    
-  }else {
+
+  } else {
     stop("Output not supported")
   }
-  
-  
- return(list(x=x,
-                weight=weight,
-                mixed_aggregation=NULL,
-                unit=units,
-                description=" Value of production for individual crops"))
-}
 
+
+ return(list(x = x,
+                weight = weight,
+                mixed_aggregation = NULL,
+                unit = units,
+                description = " Value of production for individual crops"))
+}
