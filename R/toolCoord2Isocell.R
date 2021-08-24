a9a41dde327f856bbe230824369a83f1de88dad8
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2021-08-24 14:39:35 +0200

    tweaked toolCoord2Isocell for better performance and memory efficiency in case of big data objects

--- R/toolCoord2Isocell.R
+++ R/toolCoord2Isocell.R
@@ -4,7 +4,7 @@
 #' @param x object to be transformed from coordinates to (old) magpie isocell standard
 #'
 #' @return magpie object with 59199 cells in isocell naming
-#' @author Kristine Karstens, Felicitas Beier
+#' @author Kristine Karstens, Felicitas Beier, Jan Philipp Dietrich
 #' 
 #' @param cells Switch between "magpiecell" (59199) and "lpjcell" (67420)
 #' @importFrom magpiesets addLocation
@@ -14,20 +14,15 @@
 #' @export
 
 toolCoord2Isocell <- function(x, cells="magpiecell") {
-  
-  # check if hasCoords works for more than x.y (e.g. x.y.iso) spatial dim names
-  # if(!hasCoords(x)) stop()
-  
+
   if(cells=="magpiecell"){
     
     removedim <- setdiff(unlist(strsplit(names(getItems(x))[1],"\\.")), c("x","y"))
     x <- collapseDim(x, dim = removedim)
     x <- addLocation(x)
-    x <- x["NA",,,invert=TRUE] 
     x <- collapseDim(x, dim=c("x","y"))
-    x <- toolOrderCells(x)
-    if(length(getCells(x)) != 59199) stop("Some cells out of the 59199 standard cells are missing for this data set.
-                                        Please first expand your object to cover all needed cells.")
+    x <- toolOrderCells(x, na.rm = TRUE)
+    if(length(getCells(x)) != 59199) warning("Some cells out of the 59199 standard cells are missing.")
   } else if(cells=="lpjcell"){
     
     getItems(x, dim = "cell",   maindim = 1) <- 1:67420
