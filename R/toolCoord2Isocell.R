8c7517677e41ea4d0f011d7a8135e072fd1fddee
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2021-08-24 15:51:30 +0200

    performance tweak in toolCoord2Isocell

--- R/toolCoord2Isocell.R
+++ R/toolCoord2Isocell.R
@@ -1,34 +1,38 @@
 #' @title       toolCoord2Isocell
 #' @description Transforms an object with coordinate spatial data (on half-degree) to isocell (59199) standard
-#' 
+#'
 #' @param x object to be transformed from coordinates to (old) magpie isocell standard
 #'
 #' @return magpie object with 59199 cells in isocell naming
 #' @author Kristine Karstens, Felicitas Beier, Jan Philipp Dietrich
-#' 
+#'
 #' @param cells Switch between "magpiecell" (59199) and "lpjcell" (67420)
 #' @importFrom magpiesets addLocation
 #' @importFrom madrat toolOrderCells
 #' @importFrom magclass collapseDim
-#' 
+#'
 #' @export
 
-toolCoord2Isocell <- function(x, cells="magpiecell") {
+toolCoord2Isocell <- function(x, cells = "magpiecell") {
 
-  if(cells=="magpiecell"){
-    
-    removedim <- setdiff(unlist(strsplit(names(getItems(x))[1],"\\.")), c("x","y"))
+  if (cells == "magpiecell") {
+
+    removedim <- setdiff(unlist(strsplit(names(getItems(x))[1], "\\.")), c("x", "y"))
     x <- collapseDim(x, dim = removedim)
     x <- addLocation(x)
-    x <- collapseDim(x, dim=c("x","y"))
+    x <- collapseDim(x, dim = c("x", "y"))
+    getItems(x, dim = 1.2)[getItems(x, dim = 1.2) == "0"] <- "NA"
     x <- toolOrderCells(x, na.rm = TRUE)
-    if(length(getCells(x)) != 59199) warning("Some cells out of the 59199 standard cells are missing.")
-  } else if(cells=="lpjcell"){
-    
+    if (length(getCells(x)) != 59199) warning("Some cells out of the 59199 standard cells are missing.")
+
+  } else if (cells == "lpjcell") {
+
     getItems(x, dim = "cell",   maindim = 1) <- 1:67420
-    x <- collapseDim(x, dim = c("x","y"))
-    
-  } else {stop("Unknown cells argument.")}
-  
+    x <- collapseDim(x, dim = c("x", "y"))
+
+  } else {
+stop("Unknown cells argument.")
+}
+
   return(x)
 }
