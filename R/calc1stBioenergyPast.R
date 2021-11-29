0041fd9d6e6a7e564f7ea10804e31523dec8d792
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2021-11-29 09:28:42 +0100

    - removed setConfig/getConfig where possible - updated madrat.R

--- R/calc1stBioenergyPast.R
+++ R/calc1stBioenergyPast.R
@@ -1,31 +1,28 @@
 #' @title calc1stBioenergyPast
-#' @description 
-#' Calculates first generation biofuels production, imports, exports for biogas, bioethanol and biodiesel from IEA database. 
-#' The unit is Petajoule. 
+#' @description
+#' Calculates first generation biofuels production, imports, exports for biogas, bioethanol and biodiesel
+#' from IEA database. The unit is Petajoule.
 #'
 #' @return List of magpie objects with results on country level, weight on country level, unit and description.
 #' @author Xiaoxi Wang, Isabelle Weindl
-#' @seealso
-#' [setConfig()]
 #' @examples
-#' 
-#' \dontrun{ 
+#' \dontrun{
 #' calcOutput("1stBioenergyPast")
 #' }
 #' @export
 
-calc1stBioenergyPast <- function(){
+calc1stBioenergyPast <- function() {
   df <- toolCalcIEAfromStructureMappingPEFE(
-    readSource("IEA",subtype="EnergyBalances"),
+    readSource("IEA", subtype = "EnergyBalances"),
     toolGetMapping(type = "sectoral", name = "structuremappingPE.csv", returnPathOnly = TRUE),
-    subtype="magpie")
-  
-  #Unit conversion from ktoe to PJ
-  df <- df[,,c("biogas","ethanol","oils","woodfuel")]*0.041868
-  
-  return(list(x=df,
-              weight=NULL,
-              unit="PJ",
-              description="1st generation bionegy demand")
+    subtype = "magpie")
+
+  # Unit conversion from ktoe to PJ
+  df <- df[, , c("biogas", "ethanol", "oils", "woodfuel")] * 0.041868
+
+  return(list(x = df,
+              weight = NULL,
+              unit = "PJ",
+              description = "1st generation bionegy demand")
   )
 }
