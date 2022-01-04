4a6e7ccd43052386aeb0299148cdacbc08b34e24
Felicitas <beier@pik-potsdam.de>
2022-01-04 13:57:17 +0100

    Introduced new (additional) harmonization method in toolHarmonize2Baseline (options are now: additive, multiplicative and limited)

--- R/calcLPJmL.R
+++ R/calcLPJmL.R
@@ -1,6 +1,6 @@
 #' @title calcLPJmL
 #' @description Handle LPJmL data and its time behaviour (averaging, approximation, harmonizing to baseline)
-#' 
+#'
 #' @param version Switch between LPJmL4 and LPJmL4
 #' @param climatetype Switch between different climate scenarios (default: "CRU_4")
 #' @param subtype Switch between different lpjml input as specified in readLPJmL
@@ -13,69 +13,74 @@
 #' @param ref_year just specify for harmonize_baseline != FALSE : Reference year
 #' @param limited  just specify for harmonize_baseline != FALSE : if TRUE limited approached is used
 #' @param hard_cut just specify for harmonize_baseline != FALSE : use hard cut instead of multiplicative factor
-#' 
+#'
 #' @return List of magpie objects with results on cellular level, weight, unit and description.
 #' @author Kristine Karstens, Felicitas Beier
 #' @seealso
 #' [readLPJmL()]
 #' @examples
-#' 
-#' \dontrun{ 
-#' calcOutput("LPJmL", version="LPJmL4", climatetype="CRU_4", subtype="soilc", aggregate=FALSE)
+#' \dontrun{
+#' calcOutput("LPJmL", version = "LPJmL4", climatetype = "CRU_4", subtype = "soilc", aggregate = FALSE)
 #' }
+#'
+calcLPJmL <- function(version = "LPJmL4", climatetype = "CRU_4", subtype = "soilc", subdata = NULL, time = "raw", averaging_range = NULL, dof = NULL,
+                      harmonize_baseline = FALSE, ref_year = "y2015", limited = TRUE, hard_cut = FALSE, selectyears = "all") {
 
-calcLPJmL <- function(version="LPJmL4", climatetype="CRU_4", subtype="soilc", subdata=NULL, time="raw", averaging_range=NULL, dof=NULL, 
-                      harmonize_baseline=FALSE, ref_year="y2015", limited=TRUE, hard_cut=FALSE, selectyears="all"){
-
-  if(harmonize_baseline!=FALSE){
-    
-    if(harmonize_baseline==climatetype) stop("Climatetype and baseline are identical.")
-    
-    #read in historical data for subtype
-    x           <- calcOutput("LPJmL", version=version, climatetype=climatetype, subtype=subtype, subdata=subdata, time=time, 
-                              averaging_range=averaging_range, dof=dof, harmonize_baseline=FALSE, aggregate=FALSE)
-
-    Baseline    <- calcOutput("LPJmL", version=version, climatetype=harmonize_baseline, subtype=subtype, subdata=subdata, time=time, 
-                              averaging_range=averaging_range, dof=dof, harmonize_baseline=FALSE, aggregate=FALSE)
-    #harmonize to baseline
-    LPJmL_input <- toolHarmonize2Baseline(x, Baseline,  ref_year=ref_year, limited=limited, hard_cut=hard_cut)
-    
+  if (limited) {
+    method <- "limited"
   } else {
-    
-    readin_name <- paste0(version,":",climatetype,".",subtype)  
-    LPJmL_input <- readSource("LPJmL", subtype=readin_name, convert="onlycorrect")
-    
-    if(!is.null(subdata)){
-      if(!all(subdata %in% getNames(LPJmL_input))) stop(paste0("Subdata items '", subdata,"' are not part of selected LPJmL subtype!"))
-      LPJmL_input <- LPJmL_input[,,subdata]
+    method <- "multiplicative"
+  }
+
+  if (harmonize_baseline != FALSE) {
+
+    if (harmonize_baseline == climatetype) stop("Climatetype and baseline are identical.")
+
+    # read in historical data for subtype
+    x           <- calcOutput("LPJmL", version = version, climatetype = climatetype, subtype = subtype, subdata = subdata, time = time,
+                              averaging_range = averaging_range, dof = dof, harmonize_baseline = FALSE, aggregate = FALSE)
+
+    Baseline    <- calcOutput("LPJmL", version = version, climatetype = harmonize_baseline, subtype = subtype, subdata = subdata, time = time,
+                              averaging_range = averaging_range, dof = dof, harmonize_baseline = FALSE, aggregate = FALSE)
+    # harmonize to baseline
+    LPJmL_input <- toolHarmonize2Baseline(x, Baseline,  ref_year = ref_year, method = method, hard_cut = hard_cut)
+
+  } else {
+
+    readin_name <- paste0(version, ":", climatetype, ".", subtype)
+    LPJmL_input <- readSource("LPJmL", subtype = readin_name, convert = "onlycorrect")
+
+    if (!is.null(subdata)) {
+      if (!all(subdata %in% getNames(LPJmL_input))) stop(paste0("Subdata items '", subdata, "' are not part of selected LPJmL subtype!"))
+      LPJmL_input <- LPJmL_input[, , subdata]
     }
-    
-    if("y2099" %in% (y <- getYears(LPJmL_input))){
-      LPJmL_input <- LPJmL_input[,tail(y, length(1980:2099)),] #crop GCM data to shorter time periods
+
+    if ("y2099" %in% (y <- getYears(LPJmL_input))) {
+      LPJmL_input <- LPJmL_input[, tail(y, length(1980:2099)), ] # crop GCM data to shorter time periods
     } else {
-      LPJmL_input <- LPJmL_input[,1931:as.numeric(substring(tail(y,1),2)),] #crop CRU data to shorter time periods
+      LPJmL_input <- LPJmL_input[, 1931:as.numeric(substring(tail(y, 1), 2)), ] # crop CRU data to shorter time periods
     }
-    
-    if(time=="average"){
-      
+
+    if (time == "average") {
+
       LPJmL_input <- toolTimeAverage(LPJmL_input, averaging_range = averaging_range)
-      
-    } else if(time=="spline"){
-      
+
+    } else if (time == "spline") {
+
       LPJmL_input <- toolTimeSpline(LPJmL_input, dof = dof)
-      if("y2099" %in%getYears(LPJmL_input)) LPJmL_input <- toolFillYears(LPJmL_input, c(getYears(LPJmL_input, as.integer=TRUE)[1]:2100))
-      
-    } else if(time!="raw"){
-      
+      if ("y2099" %in% getYears(LPJmL_input)) LPJmL_input <- toolFillYears(LPJmL_input, c(getYears(LPJmL_input, as.integer = TRUE)[1]:2100))
+
+    } else if (time != "raw") {
+
       stop("Time argument not supported!")
     }
   }
-  
-  if(selectyears!="all"){
-    years       <- sort(findset(selectyears,noset = "original"))
-    LPJmL_input <- LPJmL_input[,years,]
-  } 
-  
+
+  if (selectyears != "all") {
+    years       <- sort(findset(selectyears, noset = "original"))
+    LPJmL_input <- LPJmL_input[, years, ]
+  }
+
   # unit table for subtypes from readLPJmL
   units <- c(soilc              = "tC/ha",
              soilc_layer        = "tC/ha",
@@ -104,23 +109,22 @@ calcLPJmL <- function(version="LPJmL4", climatetype="CRU_4", subtype="soilc", su
              input_lake         = "mio. m^3",
              input_lake_lpjcell = "mio. m^3",
              mtranspiration     = "m^3/ha",
-             mdischarge         = "mio. m^3", 
-             mdischarge_lpjcell = "mio. m^3", 
-             mrunoff            = "mio. m^3", 
-             mrunoff_lpjcell    = "mio. m^3", 
+             mdischarge         = "mio. m^3",
+             mdischarge_lpjcell = "mio. m^3",
+             mrunoff            = "mio. m^3",
+             mrunoff_lpjcell    = "mio. m^3",
              mevaporation       = "m^3/ha",
              vegc_grass         = "tC/ha",
              litc_grass         = "tC/ha",
              soilc_grass        = "tC/ha"
   )
-  
-  unit <- toolSubtypeSelect(subtype,units)
-  
+
+  unit <- toolSubtypeSelect(subtype, units)
+
   return(list(
-    x=LPJmL_input,
-    weight=NULL,
-    unit=unit, 
-    description=paste0("Carbon output from LPJmL (",subtype,") for ", version, " and ", climatetype, "."),
-    isocountries=FALSE))
+    x = LPJmL_input,
+    weight = NULL,
+    unit = unit,
+    description = paste0("Carbon output from LPJmL (", subtype, ") for ", version, " and ", climatetype, "."),
+    isocountries = FALSE))
 }
-  
--- R/toolHarmonize2Baseline.R
+++ R/toolHarmonize2Baseline.R
@@ -3,89 +3,98 @@
 #' @param x magclass object that should be set on baseline
 #' @param base magclass object for baseline
 #' @param ref_year Reference year
-#' @param limited if TRUE, for an underestimated baseline the signal is limited to the additive term rather than the multiplicative factor
-#'                if FALSE, x will be put on the baseline as a pure multiplicative factor
+#' @param method additive: x is harmonized to base by additive factor
+#'               multiplicative: x is harmonized to base by multiplicative factor
+#'               limited: multiplicative harmonization,
+#'               but for an underestimated baseline the signal is
+#'               limited to the additive term rather than the multiplicative factor
 #' @param hard_cut Switch to TRUE for data that can not be harmonized, but have to be glued together
 #'
 #' @return the averaged data in magclass format
-#' @author Kristine Karstens
+#' @author Kristine Karstens, Felicitas Beier
 #'
 #' @export
 
-toolHarmonize2Baseline <- function(x, base, ref_year="y2015", limited=TRUE, hard_cut=FALSE){
-  
-  if(!is.magpie(x) | !is.magpie(base)) stop("Input is not a MAgPIE object, x has to be a MAgPIE object!")
-  
+toolHarmonize2Baseline <- function(x, base, ref_year = "y2015", method = "limited", hard_cut = FALSE) {
+
+  if (!is.magpie(x) | !is.magpie(base)) stop("Input is not a MAgPIE object, x has to be a MAgPIE object!")
+
   # check for negative range of values
-  negative <- (any(x<0) | any(base<0))
-  
+  negative <- (any(x < 0) | any(base < 0))
+
   # check if years are overlapping and refs is part of both time horizons
-  if(!ref_year%in%intersect(getYears(x),getYears(base))) stop("Overlapping time period of baseline and data is not including the reference year!")
-  
+  if (!ref_year %in% intersect(getYears(x), getYears(base))) stop("Overlapping time period of baseline and data is not including the reference year!")
+
   # set years
-  years       <- sort(union(getYears(base),getYears(x)))
-  till_ref    <- getYears(base, as.integer=TRUE)
-  till_ref    <- paste0("y",till_ref[till_ref <= as.numeric(substring(ref_year,2))])
-  after_ref   <- getYears(x, as.integer=TRUE)
-  after_ref   <- paste0("y",after_ref[after_ref > as.numeric(substring(ref_year,2))])
-  
-  
-  #check if x and base are identical in dimension except time
-  if(!setequal(getCells(x),getCells(base)) | !setequal(getNames(x),getNames(base))) stop("Dimensions of the MAgPIE objects do not match!")
-  
+  years       <- sort(union(getYears(base), getYears(x)))
+  till_ref    <- getYears(base, as.integer = TRUE)
+  till_ref    <- paste0("y", till_ref[till_ref <= as.numeric(substring(ref_year, 2))])
+  after_ref   <- getYears(x, as.integer = TRUE)
+  after_ref   <- paste0("y", after_ref[after_ref > as.numeric(substring(ref_year, 2))])
+
+
+  # check if x and base are identical in dimension except time
+  if (!setequal(getCells(x), getCells(base)) | !setequal(getNames(x), getNames(base))) stop("Dimensions of the MAgPIE objects do not match!")
+
   # create new magpie object with full time horizon
-  full <- new.magpie(getCells(x), years, getNames(x), sets=getSets(x))
+  full <- new.magpie(getCells(x), years, getNames(x), sets = getSets(x))
 
   full <- as.array(full)
   x    <- as.array(x)
   base <- as.array(base)
-  
+
   # from start until ref_year, use the corresponding ref value
-  full[,till_ref,]      <- base[,till_ref,]
-  
-  if(hard_cut){
-    
+  full[, till_ref, ]      <- base[, till_ref, ]
+
+  if (hard_cut) {
+
     ###########################################
     ### Use GCM data after historical data  ###
     ### from reference year +1 on           ###
-    ###########################################  
-    
-    full[,after_ref,] <- x[,after_ref,]
-    
-  } else if(limited==FALSE){
-    
-    full[,after_ref,]       <- base[,rep(ref_year,length(after_ref)),] * (x[,after_ref,] / x[,rep(ref_year,length(after_ref)),])
-    
-    # correct NAs and infinite
-    full[,after_ref,][!is.finite(full[,after_ref,])] <- (base[,rep(ref_year,length(after_ref)),]+x[,after_ref,])[!is.finite(full[,after_ref,])]  # does this make sense?
+    ###########################################
 
-  } else {
-    
+    full[, after_ref, ] <- x[, after_ref, ]
+
+  } else if (method == "multiplicative") {
+
+    full[, after_ref, ]       <- x[, after_ref, ] * (base[, rep(ref_year, length(after_ref)), ] / x[, rep(ref_year, length(after_ref)), ])
+
+    # correct NAs and infinite
+    full[, after_ref, ][!is.finite(full[, after_ref, ])] <- (base[, rep(ref_year, length(after_ref)), ] + x[, after_ref, ])[!is.finite(full[, after_ref, ])]  # does this make sense?
+
+  } else if (method == "additive") {
+
+    full[, after_ref, ]       <- x[, after_ref, ] + (base[, rep(ref_year, length(after_ref)), ] - x[, rep(ref_year, length(after_ref)), ])
+
+  } else if (method == "limited") {
+
     ###########################################
     ### Use DELTA-approach to put signal of ###
     ### GCM data on historical observation  ###
     ### data from reference year +1 on      ###
     ###########################################
-    
-    lambda <- sqrt(x[,ref_year,, drop=FALSE] / base[,ref_year,, drop=FALSE])
+
+    lambda <- sqrt(x[, ref_year, , drop = FALSE] / base[, ref_year, , drop = FALSE])
     lambda[base[, ref_year, ] <= x[, ref_year, ]] <- 1
     lambda[is.nan(lambda)] <- 1
-    
-    full[,after_ref,] <- base[,rep(ref_year,length(after_ref)),] * (1 + (x[,after_ref,] - x[,rep(ref_year,length(after_ref)),])/base[,rep(ref_year,length(after_ref)),] * 
-                                           (base[,rep(ref_year,length(after_ref)),]/x[,rep(ref_year,length(after_ref)),]) ** lambda[,rep(ref_year,length(after_ref)),])
-    
-    full[,after_ref,][is.na(full[,after_ref,])]  <- 0
-    
+
+    full[, after_ref, ] <- base[, rep(ref_year, length(after_ref)), ] * (1 + (x[, after_ref, ] - x[, rep(ref_year, length(after_ref)), ]) / base[, rep(ref_year, length(after_ref)), ] *
+                                           (base[, rep(ref_year, length(after_ref)), ] / x[, rep(ref_year, length(after_ref)), ])**lambda[, rep(ref_year, length(after_ref)), ])
+
+    full[, after_ref, ][is.na(full[, after_ref, ])]  <- 0
+
+  } else {
+    stop("Please select harmonization method (additive, multiplicative, limited (default))")
   }
-  
-  #check for nans and more
-  if(any(is.infinite(full) | is.nan(full) | is.na(full))) warning("Data containing inconsistencies.")
-  if(!negative & any(full<0)){
-    vcat(2, paste0("toolHarmonize2Baseline created unwanted negativities in the range of ", range(full[which(full<0)]), ". They will be set to zero."))
-    full[full<0] <- 0
-  } 
-  
-  out <- as.magpie(full, spatial=1)
-  
+
+  # check for nans and more
+  if (any(is.infinite(full) | is.nan(full) | is.na(full))) warning("Data containing inconsistencies.")
+  if (!negative & any(full < 0)) {
+    vcat(2, paste0("toolHarmonize2Baseline created unwanted negativities in the range of ", range(full[which(full < 0)]), ". They will be set to zero."))
+    full[full < 0] <- 0
+  }
+
+  out <- as.magpie(full, spatial = 1)
+
   return(out)
 }
