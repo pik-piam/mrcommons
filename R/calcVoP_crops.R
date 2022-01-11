bedb962f38b41244c95964611c8d5015c5ba77c8
Debbora Leip <leip@pik-potsdam.de>
2022-01-11 17:49:35 +0100

    updated units in calcVoP functions

--- R/calcVoP_crops.R
+++ R/calcVoP_crops.R
@@ -2,15 +2,10 @@
 #' @description Calculates the value of production of individual production items or
 #' its fraction compared to overall Value of Production (Agriculture,Fish,Forestry).
 #'
-#'
-#'
-#'
 #' @param output defines if output should be given as an "absolute" value or
 #' as a "fraction" of the overall value of production.
 #' @return magpie object. in mio. USD05 MER or fraction
-#' @author Edna J. Molina Bacca
-#' @importFrom dplyr mutate
-#' @importFrom luscale speed_aggregate
+#' @author Edna J. Molina Bacca, Debbora Leip
 #' @importFrom dplyr intersect
 #' @importFrom GDPuc convertGDP
 #'
@@ -26,14 +21,15 @@ calcVoP_crops <- function(output = "absolute") {
   VoP_AFF <- calcOutput("VoP_AFF", aggregate = FALSE)
   VoP_Total <- dimSums(VoP_AFF, dim = 3) # mio. 05USD MER
 
-  # Value of production of individual items (US$PPP2015 -> US$MER05)
-  item <- "Gross_Production_Value_(constant_2014_2016_thousand_I$)_(1000_Int_$)"
-  VoP_All_ppp <- readSource("FAO_online", "ValueOfProd")[, , item] / 1000
-  VoP_All <- convertGDP(VoP_All_ppp,
-                        unit_in = "constant 2015 Int$PPP",
+  # Value of production of individual items (current US$MER -> US$MER05)
+  item <- "Gross_Production_Value_(current_thousand_US$)_(1000_US$)"
+  VoP_All_currentMER <- readSource("FAO_online", "ValueOfProd")[, , item] / 1000 # mio. current US$MER
+  VoP_All <- convertGDP(VoP_All_currentMER,
+                        unit_in = "current US$MER",
                         unit_out = "constant 2005 US$MER")
-  # for countries with missing conversion factor we assume PPP = MER and no inflation:
-  VoP_All[is.na(VoP_All)] <- VoP_All_ppp[is.na(VoP_All)]
+  # for countries with missing inflation factors we assume no inflation:
+  VoP_All[is.na(VoP_All)] <- VoP_All_currentMER[is.na(VoP_All)]
+
   getNames(VoP_All) <- gsub("\\..*", "", getNames(VoP_All))
   getNames(VoP_All)[getNames(VoP_All) == "254|Oil palm fruit"] <- "254|Oil, palm fruit"
 
