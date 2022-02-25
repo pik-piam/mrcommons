694289e14c1cc12add7f27cb41ffa3fb2169fcae
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2022-02-25 11:56:28 +0100

    filter_ -> filter in CEDS

--- R/readCEDS.R
+++ R/readCEDS.R
@@ -1,4 +1,5 @@
 #' @importFrom dplyr filter_ full_join
+#' @importFrom rlang .data
 readCEDS <- function(subtype) {
   files <- c(BC    = "CEDS_BC_emissions_by_country_CEDS_sector_v_10_25_2016.csv",
              CO    = "CEDS_CO_emissions_by_country_CEDS_sector_v_10_25_2016.csv",
@@ -14,7 +15,7 @@ readCEDS <- function(subtype) {
   emi  <- read.csv(toolSubtypeSelect(subtype, files))
 
   emiLu <- read.csv("biomassburning_emissions_by_country_v1.0_ceds_country.csv")
-  emiLu <- filter_(emiLu, ~ em == subtype)
+  emiLu <- filter(emiLu, .data$em == subtype)
   dimnames(emiLu)[[2]] <- gsub("X", "", dimnames(emiLu)[[2]])
 
   dimnames(emi)[[2]] <- gsub("X", "", dimnames(emi)[[2]])
