79b256dd0aa369b3ac06aff77f807a67ca2a9484
David Chen <davidch@lt916.pik-potsdam.de>
2022-03-01 16:36:59 +0100

    read ICP food expenditures dataset

---
+++ R/convertICP2017.R
@@ -0,0 +1,48 @@
+#' @title convertICP2017
+#' @description  converts data of World Bank ICP round, downloaded from here: https://databank.worldbank.org/source/icp-2017
+#' mainly a currency conversion to MER05. Assume that in the original dataset, 2011 values are in 2011MER and 2017 in 2017MER, given the ICP rounds
+#'
+#' @param subtype data subtype needed. Either "priceLevel", or "per_cap_exp" 
+#' @return magpie object of relative price levels (world = 100) or per capita expenditure (USD17 MER)
+#' @author David M Chen
+#' @examples
+#' \dontrun{
+#' a <- convertSource("ICP2017", "per_cap_exp")
+#' }
+#'
+convertICP2017 <- function(subtype = "per_cap_expMER") {
+
+
+  #remove aggregate categories
+  x <- x[,,c("GROSS DOMESTIC PRODUCT", "FOOD AND NON-ALCOHOLIC BEVERAGES", "FOOD", "NON-ALCOHOLIC BEVERAGES")]
+  
+  #currency convert for expenditures
+  if (subtype == "per_cap_expMER"){
+
+    x2011 <- GDPuc::convertGDP(x[,2011,], unit_in = "constant 2011 US$MER", unit_out = "constant 2005 US$MER")
+    x2017 <- GDPuc::convertGDP(x[,2017,], unit_in = "constant 2017 US$MER", unit_out = "constant 2005 US$MER")
+    x <- mbind(x2011, x2017)
+    x[is.na(x)] <- 0
+  }
+  
+  #currency convert for expenditures
+  if (subtype == "per_cap_expPPP"){
+    
+    x2011 <- GDPuc::convertGDP(x[,2011,], unit_in = "constant 2011 US$PPP", unit_out = "constant 2005 US$MER")
+    x2017 <- GDPuc::convertGDP(x[,2017,], unit_in = "constant 2017 US$PPP", unit_out = "constant 2005 US$MER")
+    x <- mbind(x2011, x2017)
+    x[is.na(x)] <- 0
+  }
+  
+  #remove aggregate categories
+  
+  x <- x[,,c("GROSS DOMESTIC PRODUCT", 
+             "FOOD AND NON-ALCOHOLIC BEVERAGES",
+             "ALCOHOLIC BEVERAGES, TOBACCO AND NARCOTICS",
+             "HOUSEHOLDS AND NPISHS FINAL CONSUMPTION EXPENDITURE",
+             "INDIVIDUAL CONSUMPTION EXPENDITURE BY HOUSEHOLDS WITHOUT HOUSING",
+             "GENERAL GOVERNMENT FINAL CONSUMPTION EXPENDITURE" ), inv=T]
+    
+  
+  return(x)
+}
---
+++ R/readICP2017.R
@@ -0,0 +1,53 @@
+#' @title readICP2017
+#' @description  Reads data of World Bank ICP round, downloaded from here: https://databank.worldbank.org/source/icp-2017
+#' Reads  
+#'
+#' @param subtype data subtype needed. Either "priceLevel", or "per_cap_exp" 
+#' @return magpie object of relative price levels (world = 100) or per capita expenditure (USD17 MER)
+#' @author David M Chen
+#' @importFrom dplyr %>% select mutate filter relocate
+#' @importFrom tidyr pivot_longer
+#' @importFrom utils read.csv
+#' @examples
+#' \dontrun{
+#' a <- readSource("ICP2017", "per_cap_exp")
+#' }
+#'
+readICP2017 <- function(subtype = "per_cap_expMER") {
+  
+  file =  "31f72e60-7a0f-4d7e-8303-6482aedfdc6c_Data.csv"
+  
+  subtypes <- c(per_cap_expMER       = "Expenditure per capita, market exchange rate-based (US$)",
+             per_cap_expPPP    = "Expenditure per capita, PPP-based (US$)",
+             priceLevel         = "Price level index (World = 100)",
+             expRatio        =  "Expenditure component share of GDP (GDP = 100%)")
+  # Subsetting based on type of requested output
+  out <- toolSubtypeSelect(subtype, subtypes)
+  
+  
+  # Reads data
+  data <- read.csv(file = file)
+  colnames(data) <- gsub(colnames(data),pattern = ".*YR", replacement = "")
+  
+  #take out last lines which have a download tag
+  data <- data[1:(nrow(data)-5),]
+  
+  x   <-  data %>% 
+          select("Country.Code", "Series.Name", "Classification.Name", "2011.", "2012.", "2013.", "2014.", "2015.", "2016.", "2017."  ) %>% 
+          pivot_longer(names_to = "Year", cols = c(4:10)) %>% 
+          mutate("Product" = gsub(x = .data$Series.Name, pattern = ".*:", replacement = ""),
+                 "Year" = as.integer(.data$Year), 
+                  "value" = as.integer(gsub(x = .data$value, pattern = "\\.\\.", replacement = "0")), 
+                 "Region" = .data$Country.Code,
+                 "Indicator" = .data$Classification.Name,
+                 .keep = "unused") %>% 
+         filter(.data$Year %in% c(2011, 2017)) %>% 
+         relocate(.data$value, .after = .data$Indicator) %>% 
+        as.magpie(spatial= 3, temporal = 1, tidy = TRUE)
+  
+  #remove aggregate countries with toolCountryFill
+  x <- toolCountryFill(x, fill = 0)        
+ 
+    return(x[,,out])
+  }
+  
