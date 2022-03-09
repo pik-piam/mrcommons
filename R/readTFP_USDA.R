a31d71ca8ef997d11f8736aa0b856ecbfa325466
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2022-03-09 11:43:30 +0100

    fix linter warnings for readTFP_USDA

--- R/readTFP_USDA.R
+++ R/readTFP_USDA.R
@@ -1,72 +1,61 @@
 #' @title readTFP_USDA
 #' @description Reads the input shares from USDA's Agricultural total factor productivity growth indices assessment.
-#' 
-#'
-#'
-#'
 #'
 #' @return magpie object with fractions of different input factors in the overall production value
 #' @author Edna J. Molina Bacca
-#' @importFrom readxl read_excel
-#' @importFrom stats reshape
-#' @seealso [readSource()]
 #' @examples
-#'
 #' \dontrun{
 #' a <- readSource("TFP_USD")
-
 #' }
-#'
-readTFP_USDA <- function() {
+#' @seealso [readSource()]
+#' @importFrom readxl read_excel
+#' @importFrom stats reshape
+readTFP_USDA <- function() { # nolint
+  dataFile <- "AgTFPindividualcountries.xlsx"
 
- #File
-  file<-"AgTFPindividualcountries.xlsx"
+  countries <- read_excel(dataFile, sheet = "Factor Shares", range = "D3:D190", progress = FALSE)
+  countriesFAON <- read_excel(dataFile, sheet = "Factor Shares", range = "B3:B190", progress = FALSE)
+  regions <- cbind(countries, countriesFAON)
 
-  #Reads countries from the file
-  countries<-read_excel(file, sheet = "Factor Shares",range = "D3:D190")
-  FAON_countries<-read_excel(file, sheet = "Factor Shares",range = "B3:B190")
-  regions <- cbind(countries,FAON_countries)
+  # Available shares and their location in the dataFile
+  names <- c("revenue", "AG_Labour", "AG_Land", "Livestock", "Machinery", "Materials_Crops", "Materials_Animals")
+  ranges <- c("K3:P190", "R3:W190", "Y3:AD190", "AF3:AK190", "AM3:AR190", "AT3:AY190", "BA3:BF190")
 
-  #Available shares and their location in the file
-  names<-c("revenue","AG_Labour","AG_Land","Livestock","Machinery","Materials_Crops","Materials_Animals")
-  ranges<-c("K3:P190","R3:W190","Y3:AD190","AF3:AK190","AM3:AR190","AT3:AY190","BA3:BF190")
+  # Function to extract the values of the shares and organize the data in a format easy to convert to a magpie object
+  extractFractions <- function(names = names, ranges = ranges, file = dataFile, regions = regions) {
+    x <- NULL
+    yearsS <- as.character(seq(from = 1960, to = 2010, by = 10))
 
-  #Function to extract the values of the shares and organize the data in a format easy to convert to a magpie object
-   extract_fractions<-function(names=names,ranges=ranges,file=file,regions=regions){
-     data<-NULL
-     years_s<-as.character(seq(from = 1960, to = 2010, by = 10))
-     
-     for (n in 1:length(names)){
-       data_int<-read_excel(file, sheet = "Factor Shares",range = ranges[n])
-       colnames(data_int)<-years_s
-       data_int<-cbind(regions,data_int)
-       data_int$Input<-names[n]
-       data<-rbind(data,data_int)
-     }
-     data <- reshape(data, varying = years_s, direction = "long",idvar = c("Country/territory","FAO N","Input"),v.names = "Value",timevar = "Year",times = years_s)
-     rownames(data)<-1:length(rownames(data))
-     colnames(data)<-c("Country","CountryCode","Input","Year","Value")
-     return(data)
-     }
-  
-  #Extracts data
-  data <- extract_fractions(names,ranges,file,regions)
+    for (n in seq_along(names)) {
+      dataInt <- read_excel(dataFile, sheet = "Factor Shares", range = ranges[n], progress = FALSE)
+      colnames(dataInt) <- yearsS
+      dataInt <- cbind(regions, dataInt)
+      dataInt[["Input"]] <- names[n]
+      x <- rbind(x, dataInt)
+    }
+    x <- reshape(x, varying = yearsS, direction = "long", idvar = c("Country/territory", "FAO N", "Input"),
+                 v.names = "Value", timevar = "Year", times = yearsS)
+    rownames(x) <- seq_along(rownames(x))
+    colnames(x) <- c("Country", "CountryCode", "Input", "Year", "Value")
+    return(x)
+  }
 
-  #Reads fao countries
-  isocode_FAO<-toolGetMapping("FAOiso_faocode.csv", where="mrcommons")
-  
-  #Merges read data with the fao mapping by country code
-  data <- merge(data,isocode_FAO,by = "CountryCode",all=FALSE)[,c("ISO3","Year","Input","Value")]
-  
-  data[data$ISO3=="XCN","ISO3"]<-"CHN"
-  
-  #Creates magpie object (184 countries)
-  x<-magpiesort(as.magpie(data,spatial=1, temporal=2, datacol=4))
-  
-  #Fills with zeros the countries that were not reported
-  x<-toolCountryFill(x, fill = 0)
+  # Extracts data
+  x <- extractFractions(names, ranges, dataFile, regions)
 
+  # Reads fao countries
+  isocodeFAO <- toolGetMapping("FAOiso_faocode.csv", where = "mrcommons")
+
+  # Merges read data with the fao mapping by country code
+  x <- merge(x, isocodeFAO, by = "CountryCode", all = FALSE)[, c("ISO3", "Year", "Input", "Value")]
+
+  x[x$ISO3 == "XCN", "ISO3"] <- "CHN"
+
+  # Creates magpie object (184 countries)
+  x <- magpiesort(as.magpie(x, spatial = 1, temporal = 2, datacol = 4))
+
+  # Fills with zeros the countries that were not reported
+  x <- toolCountryFill(x, fill = 0)
 
   return(x)
 }
-
