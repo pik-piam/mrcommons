f4941ca9ca81d855e67d430019b8df56ab27eda6
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2022-03-26 14:13:24 +0100

    - fixed "entries of x could not be mapped to a country" in convert ISIMIP via extending country-coordinates-map

--- R/toolAggregateCell2Country.R
+++ R/toolAggregateCell2Country.R
@@ -1,5 +1,5 @@
 #' toolAggregateCell2Country
-#' 
+#'
 #' Aggregate cellular data (with coordinate information) to countries and perform consistency checks
 #' @param x cellular magpie object with coordinates
 #' @param weight aggregation weight
@@ -9,20 +9,20 @@
 #' @importFrom magclass getItems
 #' @export
 
-toolAggregateCell2Country <- function(x, weight=NULL, ...){
+toolAggregateCell2Country <- function(x, weight = NULL, ...) {
 
-  map <- toolGetMappingCoord2Country()
-  
+  map <- toolGetMappingCoord2Country(extended = TRUE)
+
   unknown <- which(!(getItems(x, dim = 1) %in% map$coords))
   if (length(unknown) > 0) {
     warning(length(unknown), " entries of x could not be mapped to a country and will be ignored!")
-    x <- x[-unknown,,]
+    x <- x[-unknown, , ]
   }
-  
-  out <- toolAggregate(x, map, from = 2, partrel = TRUE, weight = weight[getItems(x,dim = 1),,])
-  
+
+  out <- toolAggregate(x, map, from = 2, partrel = TRUE, weight = weight[getItems(x, dim = 1), , ])
+
   # island states are NAs: will be set to 0
-  out <- toolCountryFill(out,...)
+  out <- toolCountryFill(out, ...)
 
   return(out)
 }
