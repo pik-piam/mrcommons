edf0cd6b32dd04aa28ba19163a753cdc956b7352
David Chen <davidch@lt916.pik-potsdam.de>
2022-05-31 13:18:34 +0200

    update readICP2017

--- R/readICP2017.R
+++ R/readICP2017.R
@@ -1,6 +1,6 @@
 #' @title readICP2017
 #' @description  Reads data of World Bank ICP round, downloaded from here: https://databank.worldbank.org/source/icp-2017
-#' Reads  
+#' Reads
 #'
 #' @param subtype data subtype to be read in. Available subtypes are:
 #' \itemize{
@@ -23,9 +23,9 @@
 #' }
 #'
 readICP2017 <- function(subtype = "per_cap_expMER") {
-  
+
   file =  "418a4224-8694-47b3-a918-5200014d1728_Data.csv"
-  
+
   subtypes <- c( priceLevel = "Price level index (World = 100)",
                  expRatio = "Expenditure component share of GDP (GDP = 100%)",
                  exp_LCU = "Expenditure (local currency units, billions)",
@@ -33,36 +33,36 @@ readICP2017 <- function(subtype = "per_cap_expMER") {
                  exp_PPP = "Expenditure, PPP-based (US$, billions)",
                  per_cap_expPPP = "Expenditure per capita, PPP-based (US$)",
                  per_cap_expMER = "Expenditure per capita, market exchange rate-based (US$)")
-  
+
   # Subsetting based on type of requested output
   out <- toolSubtypeSelect(subtype, subtypes)
-  
+
   # Reads data
   data <- read.csv(file = file)
   colnames(data) <- gsub(colnames(data),pattern = ".*YR", replacement = "")
-  
+
   #take out last lines which have a download tag
   data <- data[1:(nrow(data)-5),]
-  
-  x   <-  data %>% 
-          select("Country.Code", "Series.Name", "Classification.Name", "2011.", "2012.", "2013.", "2014.", "2015.", "2016.", "2017."  ) %>% 
-          pivot_longer(names_to = "Year", cols = c(4:10)) %>% 
+
+  x   <-  data %>%
+          select("Country.Code", "Series.Name", "Classification.Name", "2011.", "2012.", "2013.", "2014.", "2015.", "2016.", "2017."  ) %>%
+          pivot_longer(names_to = "Year", cols = c(4:10)) %>%
           mutate("Product" = gsub(x = .data$Series.Name, pattern = ".*:", replacement = ""),
-                 "Year" = as.integer(.data$Year), 
-                  "value" = as.integer(gsub(x = .data$value, pattern = "\\.\\.", replacement = "0")), 
+                 "Year" = as.integer(.data$Year),
+                  "value" = as.numeric(gsub(x = .data$value, pattern = "\\.\\.", replacement = "0")),
                  "Region" = .data$Country.Code,
                  "Indicator" = .data$Classification.Name,
-                 .keep = "unused") %>% 
-         filter(.data$Year %in% c(2011, 2017)) %>% 
-         relocate(.data$value, .after = .data$Indicator) %>% 
+                 .keep = "unused") %>%
+         filter(.data$Year %in% c(2011, 2017)) %>%
+         relocate(.data$value, .after = .data$Indicator) %>%
         as.magpie(spatial= 3, temporal = 1, tidy = TRUE)
-  
+
   #remove aggregate countries with toolCountryFill
   x <- toolCountryFill(x, fill = 0, no_remove_warning = c("BON", "EAB", "ECB",
                                                           "KSV", "LCB", "MEB",
                                                           "NAB", "SAB", "SSB",
-                                                          "WLD"))        
- 
+                                                          "WLD"))
+
     return(x[,,out])
   }
-  
+
