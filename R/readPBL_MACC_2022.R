f8117dcf6029307031b1cdfca7c3b4d149ded9c3
Michael Windisch <windisch@pik-potsdam.de>
2022-06-03 13:27:02 +0200

    add read convert for 2022 MACC

---
+++ R/readPBL_MACC_2022.R
@@ -0,0 +1,118 @@
+#' Read in PBL MAC curves from Harmsen_et_al_2022 for different subtypes and scentypes
+#' 
+#' @param subtype data subtype. 
+#' "ch4coal","ch4oil","ch4gas","ch4wstl","ch4wsts","ch4rice","ch4animals","ch4anmlwst",
+#' "n2otrans","n2oadac","n2onitac","n2ofert","n2oanwst","n2owaste"
+#' @param scentype data scentype.
+#' "Default", "Optimistic", "Pessimistic"
+#' @return magpie object of the PBL_MACC_2022 data
+#' @author Michael Windisch, Florian Humpenoeder
+#' @seealso [readSource()]
+#' @importFrom data.table as.data.table
+#' @importFrom reshape2 dcast melt
+#' @importFrom readxl read_xlsx
+#' @importFrom magclass as.magpie
+readPBL_MACC_2022 <- function(subtype,scentype) {
+  
+  readMMC1 <- function(sub,scen) {
+    x <- as.data.table(read_xlsx("Data_MAC_CH4N2O_Harmsen et al_PBL.xlsx",sheet = paste0(sub,scen)))
+    x <- melt(x, id.vars = c(1,2),variable.name = "region")
+    names(x) <- c("year","steps","region","value")
+    x$type <- sub
+    x$scen <- scen
+    x$steps <- x$steps/20+1
+    x$year <- factor(x$year)
+    x <- x[,c("region","year","type","scen","steps","value")]
+    x <- as.magpie(x,spatial=1,temporal=2,tidy=TRUE)
+    names(dimnames(x))[3] <- "type.scen.steps"
+    return(x)
+  }
+  
+  
+  if(subtype=="ch4coal") {
+    x <- readMMC1("CH4_coal",paste0("_",substr(scentype,1,1)))
+    getNames(x,dim="type") <- subtype
+    getNames(x,dim="scen") <- scentype
+  }  
+  
+  if(subtype=="ch4oil") {
+    x <- readMMC1("CH4_oilp",paste0("_",substr(scentype,1,1)))
+    getNames(x,dim="type") <- subtype
+    getNames(x,dim="scen") <- scentype
+  }  
+  
+  if(subtype=="ch4gas") {
+    x <- readMMC1("CH4_ngas",paste0("_",substr(scentype,1,1)))
+    getNames(x,dim="type") <- subtype
+    getNames(x,dim="scen") <- scentype
+  }  
+  
+  if(subtype=="ch4wstl") {
+    x <- readMMC1("CH4_land",paste0("_",substr(scentype,1,1)))
+    getNames(x,dim="type") <- subtype
+    getNames(x,dim="scen") <- scentype
+  }  
+  
+  if(subtype=="ch4wsts") {
+    x <- readMMC1("CH4_sewa",paste0("_",substr(scentype,1,1)))
+    getNames(x,dim="type") <- subtype
+    getNames(x,dim="scen") <- scentype
+  }  
+  
+  if(subtype=="ch4rice") {
+    x <- readMMC1("CH4_rice",paste0("_",substr(scentype,1,1)))
+    getNames(x,dim="type") <- subtype
+    getNames(x,dim="scen") <- scentype
+  }  
+  
+  if(subtype=="ch4animals") {
+    x <- readMMC1("CH4_entf",paste0("_",substr(scentype,1,1)))
+    getNames(x,dim="type") <- subtype
+    getNames(x,dim="scen") <- scentype
+  }  
+  
+  if(subtype=="ch4anmlwst") {
+    x <- readMMC1("CH4_manu",paste0("_",substr(scentype,1,1)))
+    getNames(x,dim="type") <- subtype
+    getNames(x,dim="scen") <- scentype
+  }  
+  
+  if(subtype=="n2otrans") {
+    x <- readMMC1("N2O_tran",paste0("_",substr(scentype,1,1)))
+    getNames(x,dim="type") <- subtype
+    getNames(x,dim="scen") <- scentype
+  }  
+  
+  if(subtype=="n2oadac") {
+    x <- readMMC1("N2O_adip",paste0("_",substr(scentype,1,1)))
+    getNames(x,dim="type") <- subtype
+    getNames(x,dim="scen") <- scentype
+  }  
+  
+  if(subtype=="n2onitac") {
+    x <- readMMC1("N2O_nitr",paste0("_",substr(scentype,1,1)))
+    getNames(x,dim="type") <- subtype
+    getNames(x,dim="scen") <- scentype
+  }  
+  
+  if(subtype=="n2ofert") {
+    x <- readMMC1("N2O_fert",paste0("_",substr(scentype,1,1)))
+    getNames(x,dim="type") <- subtype
+    getNames(x,dim="scen") <- scentype
+  }  
+  
+  if(subtype=="n2oanwst") {
+    x <- readMMC1("N2O_manu",paste0("_",substr(scentype,1,1)))
+    getNames(x,dim="type") <- subtype
+    getNames(x,dim="scen") <- scentype
+  }  
+  
+  if(subtype=="n2owaste") {
+    x <- readMMC1("N2O_sewa",paste0("_",substr(scentype,1,1)))
+    getNames(x,dim="type") <- subtype
+    getNames(x,dim="scen") <- scentype
+  }
+  
+  
+  return(x)
+}
