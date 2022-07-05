5a3b2241c660939b27f542cb1f083082a8aadd5a
Kristine Karstens <karstens@pik-potsdam.de>
2022-07-05 08:37:03 +0200

    revert weights in aggregation for wood residues

--- R/convertResFor2ndBE.R
+++ R/convertResFor2ndBE.R
@@ -1,5 +1,6 @@
 #' @title convertResFor2ndBE
-#' @description Convert old ReMIND use of residues for 2nd generation bioenergy to country level data
+#' @description Convert old ReMIND use of residues for 2nd generation
+#'              bioenergy to country level data
 #'
 #' @return List of magpie objects with results on country level
 #' @param x MAgPIE object containing original values
@@ -10,22 +11,31 @@
 #' \dontrun{
 #' a <- readSource("ResFor2ndBE", subtype = "oldReMIND", convert = TRUE)
 #' }
-#'
+
 convertResFor2ndBE <- function(x, subtype = subtype) {
 
   if (subtype == "oldReMIND") {
 
-    ### start from global numbers (since old ReMIND region shares are not very sophisticated)
+    ### start from global numbers (since old REMIND region
+    ### shares are not very sophisticated)
     x <- dimSums(x, dim = 1)
     getItems(x, dim = 1) <- "GLO"
 
-    ### disaggregate residue to county level using forestry areas and agricultural production of y1995
-    landuse <- calcOutput("LanduseInitialisation", nclasses = "seven", fao_corr = TRUE, aggregate = FALSE)[,"y1995",]
-    GLO2iso <- as.data.frame(list(rep("GLO", length(getCells(landuse))), getCells(landuse)))
+    ### disaggregate residue to county level using forestry areas and
+    ### agricultural production of y1995
+    landuse <- calcOutput("LanduseInitialisation", nclasses = "seven",
+                 fao_corr = TRUE, aggregate = FALSE)[, "y1995",
+                                                       c("forestry", "crop")]
+    GLO2iso <- as.data.frame(list(rep("GLO", length(getCells(landuse))),
+                                  getCells(landuse)))
     names(GLO2iso) <- c("GLO", "iso")
 
-    res_wood <- toolAggregate(x[, , "res_wood"], rel = GLO2iso, weight = dimSums(landuse[, , c("primforest","secdforest","forestry")],dim=3), from = "GLO", to = "iso")
-    res_crop <- toolAggregate(x[, , "res_crop"], rel = GLO2iso, weight = landuse[, , "crop"], from = "GLO", to = "iso")
+    res_wood <- toolAggregate(x[, , "res_wood"], rel = GLO2iso,
+                              weight = landuse[, , "forestry"],
+                              from = "GLO", to = "iso")
+    res_crop <- toolAggregate(x[, , "res_crop"], rel = GLO2iso,
+                              weight = landuse[, , "crop"],
+                              from = "GLO", to = "iso")
     out <- toolCountryFill(mbind(res_crop, res_wood), fill = 0, verbosity = 0)
 
   } else if (subtype == "newAgriSupply") {
