752cb5f4ea4b205e222fc504ba65a4f97a957cc6
emolinab <mbacca@pik-potsdam.de>
2022-08-24 09:17:11 +0200

    broader crop model options for calcISIMIP3bYield

--- R/downloadISIMIP.R
+++ R/downloadISIMIP.R
@@ -23,80 +23,80 @@ downloadISIMIP <- function(subtype) {
 
   if (grepl("airww",subtype)) {
 
-  x <- toolSplitSubtype(subtype, list(dataset = "airww",
-                                      model   = c("CLM45", "CLM50", "CWatM", "DBH", "H08", "JULES-W1", "LPJmL", "MATSIRO", "MPI-HM", "ORCHIDEE", "ORCHIDEE-DGVM", "PCR-GLOBWB", "WaterGAP2"),
-                                      gcm     = c("gfdl-esm2m","hadgem2-es","ipsl-cm5a-lr","miroc5"),
-                                      version = c("2a","2b","3a","3b")))
+    x <- toolSplitSubtype(subtype, list(dataset = "airww",
+                                        model   = c("CLM45", "CLM50", "CWatM", "DBH", "H08", "JULES-W1", "LPJmL", "MATSIRO", "MPI-HM", "ORCHIDEE", "ORCHIDEE-DGVM", "PCR-GLOBWB", "WaterGAP2"),
+                                        gcm     = c("gfdl-esm2m","hadgem2-es","ipsl-cm5a-lr","miroc5"),
+                                        version = c("2a","2b","3a","3b")))
 
-  paths <- c(airww = paste0("ISIMIP",x$version,"/OutputData/water_global/",x$model,"/",x$gcm,"/historical/",
-                            tolower(x$model),"_",tolower(x$gcm),"_ewembi_picontrol_histsoc_co2_airrww_global_monthly_1861_2005.nc4"))
+    paths <- c(airww = paste0("ISIMIP",x$version,"/OutputData/water_global/",x$model,"/",x$gcm,"/historical/",
+                              tolower(x$model),"_",tolower(x$gcm),"_ewembi_picontrol_histsoc_co2_airrww_global_monthly_1861_2005.nc4"))
 
-  path <- toolSubtypeSelect(x$dataset,paths)
-  if (file.exists(paste0("/p/projects/isimip/isimip/",path))) {
-    storage <- "/p/projects/isimip/isimip/"
-    file.copy(paste0(storage,path), basename(path))
-  } else {
-    storage <- "https://files.isimip.org/"
-    # download the data
-    err <- try(suppressWarnings(download.file(paste0(storage,path),
-                                              destfile = basename(path), mode = "wb",
-                                              quiet = FALSE)), silent = TRUE)
-    if(inherits(err, "try-error")) stop("Data for requested subtype \"",subtype,"\" could not be found!")
-  }
-  .getMetadata <- function(dataset, version) {
-    out <- list(doi = NULL, version = NULL, title = NULL, description = NULL)
-    # Info about DOIs is available here: https://www.isimip.org/outputdata/dois-isimip-data-sets/
-    if (dataset %in% "airww" && version == "2a") {
-      out$doi         <- "http://doi.org/10.5880/PIK.2019.003"
-      out$version     <- "v1.1"
-      out$title       <- ""
-      out$description <- ""
+    path <- toolSubtypeSelect(x$dataset,paths)
+    if (file.exists(paste0("/p/projects/isimip/isimip/",path))) {
+      storage <- "/p/projects/isimip/isimip/"
+      file.copy(paste0(storage,path), basename(path))
+    } else {
+      storage <- "https://files.isimip.org/"
+      # download the data
+      err <- try(suppressWarnings(download.file(paste0(storage,path),
+                                                destfile = basename(path), mode = "wb",
+                                                quiet = FALSE)), silent = TRUE)
+      if(class(err)=="try-error") stop("Data for requested subtype \"",subtype,"\" could not be found!")
     }
-    return(out)
+    .getMetadata <- function(dataset, version) {
+      out <- list(doi = NULL, version = NULL, title = NULL, description = NULL)
+      # Info about DOIs is available here: https://www.isimip.org/outputdata/dois-isimip-data-sets/
+      if (dataset %in% "airww" && version == "2a") {
+        out$doi         <- "http://doi.org/10.5880/PIK.2019.003"
+        out$version     <- "v1.1"
+        out$title       <- ""
+        out$description <- ""
+      }
+      return(out)
+    }
+    meta1 <- .getMetadata(x$dataset, x$version)
+    meta2 <- metadataGFZ(meta1$doi)
+
+
+    # Compose meta data
+    return(list(url           = paste0(storage,path),
+                doi           = meta1$doi,
+                title         = meta1$title,
+                author        = meta2$authors,
+                version       = meta1$version,
+                release_date  = meta2$year,
+                description   = meta1$description,
+                license       = meta2$license,
+                reference     = meta2$citation)
+    )
+
   }
-  meta1 <- .getMetadata(x$dataset, x$version)
-  meta2 <- metadataGFZ(meta1$doi)
+
+  if (grepl("yields",subtype)) {
 
 
-  # Compose meta data
-  return(list(url           = paste0(storage,path),
-              doi           = meta1$doi,
-              title         = meta1$title,
-              author        = meta2$authors,
-              version       = meta1$version,
-              release_date  = meta2$year,
-              description   = meta1$description,
-              license       = meta2$license,
-              reference     = meta2$citation)
-  )
+    x <- toolSplitSubtype(subtype, list(dataset = "yields",
+                                        model   = c("LPJmL", "EPIC-IIASA", "pDSSAT", "CYGMA1p74","PROMET","CROVER","ISAM","LDNDC","PEPIC"),
+                                        gcm     = c("gfdl-esm4", "ipsl-cm6a-lr", "mpi-esm1-2-hr", "mri-esm2-0", "ukesm1-0-ll"),
+                                        scen    = c("historical", "ssp126", "ssp370", "ssp585"),
+                                        co2     = c("default", "2015co2"),
+                                        version = c("2a","2b","3a","3b")))
 
-}
+    if (x$scen == "historical") {
+      years <- "1850_2014"} else {years <- "2015_2100"}
 
-if (grepl("yields",subtype)) {
+    for (crop in c("mai", "ri1", "ri2", "swh", "wwh", "soy")){
+      for (irr in c("firr", "noirr")) {
+        paths <- c(yields = paste0(x$model,"/phase",x$version,"/",x$gcm,"/",x$scen, "/", crop, "/",
+                                   tolower(x$model),"_",tolower(x$gcm),"_w5e5_", x$scen, "_2015soc_",x$co2, "_yield-", crop, "-",irr, "_global_annual_", years,".nc"))
 
-
-  x <- toolSplitSubtype(subtype, list(dataset = "yields",
-                                      model   = c("LPJmL", "EPIC-IIASA", "pDSSAT", "CYGMA1p74","PROMET","CROVER"),
-                                      gcm     = c("gfdl-esm4", "ipsl-cm6a-lr", "mpi-esm1-2-hr", "mri-esm2-0", "ukesm1-0-ll"),
-                                      scen    = c("historical", "ssp126", "ssp370", "ssp585"),
-                                      co2     = c("default", "2015co2"),
-                                      version = c("2a","2b","3a","3b")))
-
-  if (x$scen == "historical") {
-    years <- "1850_2014"} else {years <- "2015_2100"}
-
-  for (crop in c("mai", "ri1", "ri2", "swh", "wwh", "soy")){
-    for (irr in c("firr", "noirr")) {
-      paths <- c(yields = paste0(x$model,"/phase",x$version,"/",x$gcm,"/",x$scen, "/", crop, "/",
-                                 tolower(x$model),"_",tolower(x$gcm),"_w5e5_", x$scen, "_2015soc_",x$co2, "_yield-", crop, "-",irr, "_global_annual_", years,".nc"))
-
-      path <- toolSubtypeSelect(x$dataset,paths)
-      if (file.exists(paste0("/p/projects/macmit/data/GGCMI/AgMIP.output/",path))) {
+        path <- toolSubtypeSelect(x$dataset,paths)
+        if (file.exists(paste0("/p/projects/macmit/data/GGCMI/AgMIP.output/",path))) {
           storage <- "/p/projects/macmit/data/GGCMI/AgMIP.output/"
           file.copy(paste0(storage,path), basename(path))
-         } else {
+        } else {
           vcat(1, paste0("Data for requested subtype \"",path,"\" could not be found!"))}
-    }}
+      }}
 
 
   }
