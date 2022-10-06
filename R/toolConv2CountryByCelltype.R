a444fdc3cec89beb87f35a26eedbb8f91107a9a6
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2022-10-06 16:14:48 +0200

    - calcLUH2v2 does now return identical totals for cellular TRUE and FALSE for both lpjcell and magpiecell

---
+++ R/toolConv2CountryByCelltype.R
@@ -0,0 +1,19 @@
+#' toolConv2CountryByCelltype
+#'
+#' Aggregates cellular data to ISO country level after conversion of cellular
+#' data to a specific cell setup (this type is relevant as some settings,
+#' such as "magpiecell" remove some cells and therby affect country sums)
+#' @param x magpie object on cellular level
+#' @param cells switch between 59199 ("magpiecell") and 67420 ("lpjcell") cells
+#' @return return selected input data on ISO country level
+#' @author Jan Philipp Dietrich
+
+toolConv2CountryByCelltype <- function(x, cells) {
+  getSets(x, fulldim=FALSE)[1] <- "x.y.iso"
+  out   <- toolCoord2Isocell(x, cells = cells)
+  map <- data.frame(from = getItems(out, dim = 1),
+                    to = getItems(out, dim = 1.1, full = TRUE))
+  out <- toolAggregate(out, map)
+  getSets(out, fulldim = FALSE)[1] <- "country"
+  return(toolCountryFill(out, fill = 0, verbosity = 2))
+}
