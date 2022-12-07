d42835881ae531fe42bfc90cd0a7f07f18877d86
Mika Pflüger <mika.pflueger@pik-potsdam.de>
2022-12-07 15:32:18 +0100

    toolHarmonize2Baseline.R code styling

--- R/toolHarmonize2Baseline.R
+++ R/toolHarmonize2Baseline.R
@@ -15,59 +15,64 @@
 #'
 #' @export
 
-toolHarmonize2Baseline <- function(x, base, ref_year = "y2015", method = "limited", hard_cut = FALSE) {
-
-  if (!is.magpie(x) | !is.magpie(base)) stop("Input is not a MAgPIE object, x has to be a MAgPIE object!")
+toolHarmonize2Baseline <- function(x,
+                                   base,
+                                   ref_year = "y2015", # nolint: object_name_linter
+                                   method = "limited",
+                                   hard_cut = FALSE # nolint: object_name_linter
+) {
+  if (!is.magpie(x) || !is.magpie(base)) stop("Input is not a MAgPIE object, x has to be a MAgPIE object!")
 
   # check for negative range of values
   negative <- (any(x < 0) | any(base < 0))
 
   # check if years are overlapping and refs is part of both time horizons
-  if (!ref_year %in% intersect(getYears(x), getYears(base))) stop("Overlapping time period of baseline and data is not including the reference year!")
+  if (!ref_year %in% intersect(getYears(x), getYears(base))) {
+    stop("Overlapping time period of baseline and data is not including the reference year!")
+  }
 
   # set years
-  years       <- sort(union(getYears(base), getYears(x)))
-  till_ref    <- getYears(base, as.integer = TRUE)
-  till_ref    <- paste0("y", till_ref[till_ref <= as.numeric(substring(ref_year, 2))])
-  after_ref   <- getYears(x, as.integer = TRUE)
-  after_ref   <- paste0("y", after_ref[after_ref > as.numeric(substring(ref_year, 2))])
+  years <- sort(union(getYears(base), getYears(x)))
+  tillRef <- getYears(base, as.integer = TRUE)
+  tillRef <- paste0("y", tillRef[tillRef <= as.numeric(substring(ref_year, 2))])
+  afterRef <- getYears(x, as.integer = TRUE)
+  afterRef <- paste0("y", afterRef[afterRef > as.numeric(substring(ref_year, 2))])
 
 
   # check if x and base are identical in dimension except time
-  if (!setequal(getCells(x), getCells(base)) | !setequal(getNames(x), getNames(base))) stop("Dimensions of the MAgPIE objects do not match!")
+  if (!setequal(getCells(x), getCells(base)) || !setequal(getNames(x), getNames(base))) {
+    stop("Dimensions of the MAgPIE objects do not match!")
+  }
 
   # create new magpie object with full time horizon
   full <- new.magpie(getCells(x), years, getNames(x), sets = getSets(x))
 
   full <- as.array(full)
-  x    <- as.array(x)
+  x <- as.array(x)
   base <- as.array(base)
 
   # from start until ref_year, use the corresponding ref value
-  full[, till_ref, ]      <- base[, till_ref, ]
+  full[, tillRef, ] <- base[, tillRef, ]
 
   if (hard_cut) {
-
     ###########################################
     ### Use GCM data after historical data  ###
     ### from reference year +1 on           ###
     ###########################################
 
-    full[, after_ref, ] <- x[, after_ref, ]
-
+    full[, afterRef, ] <- x[, afterRef, ]
   } else if (method == "multiplicative") {
-
-    full[, after_ref, ]       <- x[, after_ref, ] * (base[, rep(ref_year, length(after_ref)), ] / x[, rep(ref_year, length(after_ref)), ])
+    full[, afterRef, ] <- x[, afterRef, ] * (base[, rep(ref_year, length(afterRef)), ]
+    / x[, rep(ref_year, length(afterRef)), ])
 
     # correct NAs and infinite
-    full[, after_ref, ][!is.finite(full[, after_ref, ])] <- (base[, rep(ref_year, length(after_ref)), ] + x[, after_ref, ])[!is.finite(full[, after_ref, ])]  # does this make sense?
-
+    full[, afterRef, ][!is.finite(full[, afterRef, ])] <- (
+      base[, rep(ref_year, length(afterRef)), ] + x[, afterRef, ]
+    )[!is.finite(full[, afterRef, ])] # does this make sense?
   } else if (method == "additive") {
-
-    full[, after_ref, ]       <- x[, after_ref, ] + (base[, rep(ref_year, length(after_ref)), ] - x[, rep(ref_year, length(after_ref)), ])
-
+    full[, afterRef, ] <- x[, afterRef, ] + (base[, rep(ref_year, length(afterRef)), ]
+    - x[, rep(ref_year, length(afterRef)), ])
   } else if (method == "limited") {
-
     ###########################################
     ### Use DELTA-approach to put signal of ###
     ### GCM data on historical observation  ###
@@ -78,19 +83,30 @@ toolHarmonize2Baseline <- function(x, base, ref_year = "y2015", method = "limite
     lambda[base[, ref_year, ] <= x[, ref_year, ]] <- 1
     lambda[is.nan(lambda)] <- 1
 
-    full[, after_ref, ] <- base[, rep(ref_year, length(after_ref)), ] * (1 + (x[, after_ref, ] - x[, rep(ref_year, length(after_ref)), ]) / base[, rep(ref_year, length(after_ref)), ] *
-                                           (base[, rep(ref_year, length(after_ref)), ] / x[, rep(ref_year, length(after_ref)), ])**lambda[, rep(ref_year, length(after_ref)), ])
+    full[, afterRef, ] <-
+      base[, rep(ref_year, length(afterRef)), ] *
+        (1 + (x[, afterRef, ] - x[, rep(ref_year, length(afterRef)), ])
+        / base[, rep(ref_year, length(afterRef)), ]
+          * (base[, rep(ref_year, length(afterRef)), ]
+          / x[, rep(ref_year, length(afterRef)), ]
+          )**lambda[, rep(ref_year, length(afterRef)), ]
+        )
 
-    full[, after_ref, ][is.na(full[, after_ref, ])]  <- 0
-
+    full[, afterRef, ][is.na(full[, afterRef, ])] <- 0
   } else {
     stop("Please select harmonization method (additive, multiplicative, limited (default))")
   }
 
   # check for nans and more
-  if (any(is.infinite(full) | is.nan(full) | is.na(full))) warning("Data containing inconsistencies.")
-  if (!negative & any(full < 0)) {
-    vcat(2, paste0("toolHarmonize2Baseline created unwanted negativities in the range of ", range(full[which(full < 0)]), ". They will be set to zero."))
+  if (any(is.infinite(full) | is.nan(full) | is.na(full))) {
+    warning("Data containing inconsistencies.")
+  }
+  if (!negative && any(full < 0)) {
+    vcat(2, paste0(
+      "toolHarmonize2Baseline created unwanted negativities in the range of ",
+      range(full[which(full < 0)]),
+      ". They will be set to zero."
+    ))
     full[full < 0] <- 0
   }
 
