3887376d1cdbea0a7f214670f4ed0f0aedf299db
Falk Benke <falk.benke@pik-potsdam.de>
2022-12-20 15:19:35 +0100

    Revert "Use new MAgPIE variable names and calculate total costs differently than before."
    This reverts commit 0d4180ce5fc0c44b508ac3dda63d8edf50197344.

--- R/calcMAgPIEReport.R
+++ R/calcMAgPIEReport.R
@@ -6,16 +6,13 @@ calcMAgPIEReport <- function(subtype){
 
   if (subtype == "CostTotal") {
 
-    #x <- x[,,"Costs|MainSolve w/o GHG Emissions (million US$05/yr)"]/1000/1000 # with transformation factor from 10E6 US$2005 to 10E12 US$2005
-    a <- x[,,"Costs Accounting (million US$05/yr)"]/1000/1000
-    b <- x[,,"Costs Accounting|+|GHG Emissions (million US$05/yr)"]/1000/1000
-    x <- a-b
+    x <- x[,,"Costs|MainSolve w/o GHG Emissions (million US$05/yr)"]/1000/1000 # with transformation factor from 10E6 US$2005 to 10E12 US$2005
     d <- "Total Landuse Costs from MAgPIE excluding emission costs"
     u <- "T$2005/yr"
 
   } else if (subtype == "CostMAC") {
 
-    x <- x[,,"Costs Accounting|+|MACCS (million US$05/yr)"]/1000/1000 # with transformation factor from 10E6 US$2005 to 10E12 US$2005
+    x <- x[,,"Costs|MainSolve|MACCS (million US$05/yr)"]/1000/1000 # with transformation factor from 10E6 US$2005 to 10E12 US$2005
     d <- "MAC Costs for LU emissions from MAgPIE"
     u <- "T$2005/yr"
 
