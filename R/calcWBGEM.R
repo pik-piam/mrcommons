888b724a49e27a6a88f3cb6219863491ff403e49
Felicitas <beier@pik-potsdam.de>
2023-01-11 17:29:18 +0100

    Introduced unit argument in calcPriceAgriculture to allow unit in different MER PPP or base years

--- R/calcWBGEM.R
+++ R/calcWBGEM.R
@@ -1,6 +1,6 @@
 #' @title calcWBGEM
-#' @description  recover world price of commidity in terms of real 2005 USD per metric ton
-#' @return  magpie object of time series of world price of commodites
+#' @description  recover world price of commodity in terms of real 2005 USD per metric ton
+#' @return  magpie object of time series of world price of commodities
 #' @author Xiaoxi Wang
 #' @seealso
 #' [readSource()],
@@ -11,14 +11,17 @@
 #' calcOutput("WBGEM")
 #' }
 #'
-#' @importFrom magclass mbind
-
+#' @importFrom magclass mbind getNames setNames setYears
 
 calcWBGEM <- function() {
-  x <- readSource("WBGEM")
+
+  x     <- readSource("WBGEM")
   sugar <- grep("world", grep("Sugar", getNames(x), value = TRUE), value = TRUE)
-  adjustFactor2010 <- setNames(x[, , grep("real", sugar, value = TRUE)] / setNames(x[, , grep("nominal", sugar, value = TRUE)], NULL), NULL)
-  adjustFactor2005 <- 1 / setYears(x[, 2005, grep("real", sugar, value = TRUE)] / setNames(x[, 2005, grep("nominal", sugar, value = TRUE)], NULL), NULL) * adjustFactor2010
+  adjustFactor2010 <- setNames(x[, , grep("real", sugar, value = TRUE)] /
+                                 setNames(x[, , grep("nominal", sugar, value = TRUE)], NULL), NULL)
+  adjustFactor2005 <- 1 / setYears(x[, 2005, grep("real", sugar, value = TRUE)] /
+                                     setNames(x[, 2005, grep("nominal", sugar, value = TRUE)], NULL), NULL) *
+                                       adjustFactor2010
   adjustFactor2005 <- setNames(adjustFactor2005, NULL)
 
   vars <- grep("nominal", getNames(x), value = TRUE)
@@ -27,17 +30,21 @@ calcWBGEM <- function() {
   tmp3 <- x[, , grep("Agr", vars, value = TRUE)]
 
   .rename <- function(x) {
-    var_name <- getNames(x)
-    var_name <- gsub("_nominal", "", gsub("\\$", "", gsub("[{^\\#&~_/<>'!,:.;`\"}@-]", "_", gsub("[ /\t\n\r\f\v]", "", var_name))))
-    var_name <- gsub("_kg", "", var_name)
-    var_name <- gsub("_mt", "", var_name)
-    getNames(x) <- var_name
+    varName <- getNames(x)
+    varName <- gsub("_nominal", "",
+                    gsub("\\$", "",
+                         gsub("[{^\\#&~_/<>'!,:.;`\"}@-]", "_",
+                              gsub("[ /\t\n\r\f\v]", "", varName))))
+    varName <- gsub("_kg", "", varName)
+    varName <- gsub("_mt", "", varName)
+    getNames(x) <- varName
     return(x)
   }
 
   out <- mbind(.rename(tmp1), .rename(tmp2), .rename(tmp3))
   out <- out * adjustFactor2005
   out[is.na(out)] <- 0
+
   return(list(x = out,
               unit = "real2005 USD per ton",
               weight = NULL,
