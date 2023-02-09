b2ab593c18d2ad1af1e28a866fd8d075c1b5e581
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2023-02-09 16:26:04 +0100

    lintfix

--- R/convertEDGAR_LU.R
+++ R/convertEDGAR_LU.R
@@ -1,4 +1,4 @@
-convertEDGAR_LU <- function(x) {
+convertEDGAR_LU <- function(x) { # nolint: object_name_linter.
   # fill all missing countries with 0
   x <- toolCountryFill(x, fill = 0)
   return(x)
