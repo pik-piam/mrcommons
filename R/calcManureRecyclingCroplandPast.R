b1c2a7f278e2014e557e690466535d2fd7fa708f
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2023-02-09 12:20:05 +0100

    lintfix

--- R/calcManureRecyclingCroplandPast.R
+++ R/calcManureRecyclingCroplandPast.R
@@ -1,5 +1,6 @@
 #' @title calcManureRecyclingCroplandPast
-#' @description calculates manure recycling to cropland based on excretions, animal waste management types (and their shares per country) and emission factors for nitrogenous emissions in livestock confinements
+#' @description calculates manure recycling to cropland based on excretions, animal waste management types
+#' (and their shares per country) and emission factors for nitrogenous emissions in livestock confinements
 #'
 #' @param cellular if TRUE value is calculate and returned (set aggregate to FALSE!) on cellular level
 #' @param products "sum" (default) or "kli"
@@ -18,35 +19,38 @@
 calcManureRecyclingCroplandPast <- function(products = "sum", cellular = FALSE) {
 
   past               <- findset("past")
-  Excretion          <- collapseNames(calcOutput("Excretion", cellular = cellular, attributes = "npkc", aggregate = FALSE)[, past, "confinement"])
-  EmissionFactors_n  <- calcOutput("EF3confinement", selection = "recycling", aggregate = FALSE)
-  LossRates_c        <- calcOutput("ClossConfinement", aggregate = FALSE)
-  AnimalWasteMSShare <- collapseNames(calcOutput("AWMSconfShr", aggregate = FALSE)[, past, "constant"])
+  excretion          <- collapseNames(calcOutput("excretion", cellular = cellular, attributes = "npkc",
+                                                 aggregate = FALSE)[, past, "confinement"])
+  emissionFactorsN  <- calcOutput("EF3confinement", selection = "recycling", aggregate = FALSE)
+  lossRatesC        <- calcOutput("ClossConfinement", aggregate = FALSE)
+  animalWasteMSShare <- collapseNames(calcOutput("AWMSconfShr", aggregate = FALSE)[, past, "constant"])
 
   if (cellular) {
 
-    EmissionFactors_n  <- toolIso2CellCountries(EmissionFactors_n)
-    LossRates_c        <- toolIso2CellCountries(LossRates_c)
-    AnimalWasteMSShare <- toolIso2CellCountries(AnimalWasteMSShare)
+    emissionFactorsN  <- toolIso2CellCountries(emissionFactorsN)
+    lossRatesC        <- toolIso2CellCountries(lossRatesC)
+    animalWasteMSShare <- toolIso2CellCountries(animalWasteMSShare)
   }
 
   if (products == "sum") {
 
-    ManureNitrogen       <- dimSums(Excretion[, , "nr"] * AnimalWasteMSShare * EmissionFactors_n, dim = c(3.1, 3.3))
+    manureNitrogen       <- dimSums(excretion[, , "nr"] * animalWasteMSShare * emissionFactorsN, dim = c(3.1, 3.3))
     vcat(verbosity = 2, "no P and K losses in manure management assumed")
-    ManurePhosphorKalium <- dimSums(Excretion[, , c("p", "k")], dim = 3.1)
-    ManureCarbon         <- dimSums(Excretion[, , "c"] * AnimalWasteMSShare * (1 - LossRates_c), dim = c(3.1, 3.3))
+    manurePhosphorKalium <- dimSums(excretion[, , c("p", "k")], dim = 3.1)
+    manureCarbon         <- dimSums(excretion[, , "c"] * animalWasteMSShare * (1 - lossRatesC), dim = c(3.1, 3.3))
 
   } else if (products == "kli") {
 
-    ManureNitrogen       <- dimSums(Excretion[, , "nr"] * AnimalWasteMSShare * EmissionFactors_n, dim = c(3.3))
+    manureNitrogen       <- dimSums(excretion[, , "nr"] * animalWasteMSShare * emissionFactorsN, dim = c(3.3))
     vcat(verbosity = 2, "no P and K losses in manure management assumed")
-    ManurePhosphorKalium <- Excretion[, , c("p", "k")]
-    ManureCarbon         <- dimSums(Excretion[, , "c"] * AnimalWasteMSShare * (1 - LossRates_c), dim = c(3.3))
+    manurePhosphorKalium <- excretion[, , c("p", "k")]
+    manureCarbon         <- dimSums(excretion[, , "c"] * animalWasteMSShare * (1 - lossRatesC), dim = c(3.3))
 
-  } else stop(paste("Type", products, "is not a valid for parameter 'products'."))
+  } else {
+    stop(paste("Type", products, "is not a valid for parameter 'products'."))
+  }
 
-  out       <- mbind(ManureNitrogen, ManurePhosphorKalium, ManureCarbon)
+  out       <- mbind(manureNitrogen, manurePhosphorKalium, manureCarbon)
 
   return(list(x            = out,
               weight       = NULL,
