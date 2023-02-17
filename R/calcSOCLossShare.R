7dbe919e72abf57f8e439ee806079215a3d67380
Falk Benke <falk.benke@pik-potsdam.de>
2023-02-17 10:54:30 +0100

    Revert "lintfix"
    This reverts commit 4c054cf3af2540a5099df1c9c4f29644afbb74ba.

--- R/calcSOCLossShare.R
+++ R/calcSOCLossShare.R
@@ -16,49 +16,48 @@
 #' }
 #'
 calcSOCLossShare <- function(subsystems = FALSE, rate = "change", ipcc = "guide2006") {
-  years                 <- findset("past")
-  kgClimate             <- readSource("Koeppen", subtype = "cellular", convert = "onlycorrect")[, years, ]
-  kg2ipcc               <- toolGetMapping("mapping_koeppen_ipcc.csv", type = "sectoral")
-  getNames(kgClimate)   <- tolower(getNames(kgClimate))
-  kg2ipcc$koeppenGeiger <- tolower(kg2ipcc$koeppenGeiger)
-  year2climateClasses   <- c(guide2006 = "ipcc_reduced", guide2019 = "ipcc_reduced2019")
-  climateClasses        <- toolSubtypeSelect(ipcc, year2climateClasses)
 
-  ipccClimate           <- toolAggregate(kgClimate, rel = kg2ipcc,
-                                         from = "koeppenGeiger", to = climateClasses, dim = 3)
+  years                  <- findset("past")
+  KG_climate             <- readSource("Koeppen", subtype = "cellular", convert = "onlycorrect")[, years, ]
+  KG2IPCC                <- toolGetMapping("mapping_koeppen_ipcc.csv", type = "sectoral")
+  getNames(KG_climate)   <- tolower(getNames(KG_climate))
+  KG2IPCC$koeppen_geiger <- tolower(KG2IPCC$koeppen_geiger)
+  year2climateClasses    <- c(guide2006 = "ipcc_reduced", guide2019 = "ipcc_reduced2019")
+  climateClasses         <- toolSubtypeSelect(ipcc, year2climateClasses)
 
-  year2scf              <- c(guide2006 = "SCF_sub", guide2019 = "SCF_sub2019")
-  scf                   <- toolSubtypeSelect(ipcc, year2scf)
-  scfSub2ipccClimate    <- readSource("IPCC",    subtype = scf, convert = FALSE)[, , getNames(ipccClimate)]
+  IPCC_climate           <- toolAggregate(KG_climate, rel = KG2IPCC, from = "koeppen_geiger", to = climateClasses, dim = 3)
 
-  if (isFALSE(subsystems)) {
+  year2SCF               <- c(guide2006 = "SCF_sub", guide2019 = "SCF_sub2019")
+  SCF                    <- toolSubtypeSelect(ipcc, year2SCF)
+  SCFsub2IPCCclimate     <- readSource("IPCC",    subtype = SCF, convert = FALSE)[, , getNames(IPCC_climate)]
 
-    socLossShare           <- dimSums(ipccClimate * scfSub2ipccClimate[, , "maiz"], dim = 3.1)
-    getNames(socLossShare) <- "cshare"
+  if (subsystems == FALSE) {
+
+    SOCLossShare           <- dimSums(IPCC_climate * SCFsub2IPCCclimate[, , "maiz"], dim = 3.1)
+    getNames(SOCLossShare) <- "cshare"
 
   } else if (subsystems %in% c(TRUE, "aggregated")) {
 
-    socLossShare           <- dimSums(ipccClimate * scfSub2ipccClimate, dim = 3.1)
+    SOCLossShare           <- dimSums(IPCC_climate * SCFsub2IPCCclimate, dim = 3.1)
 
     if (subsystems == "aggregated") {
-      magCrop      <- calcOutput("Croparea", physical = TRUE, cellular = TRUE, irrigation = FALSE, aggregate = FALSE)
-      kcr2all      <- data.frame(list(kcr = getNames(socLossShare), all = rep("all", 19)))
-      socLossShare <- toolAggregate(socLossShare, weight = magCrop, rel = kcr2all, from = "kcr", to = "all", dim = 3)
-      getNames(socLossShare) <- "cshare"
+      MAGCrop      <- calcOutput("Croparea", physical = TRUE, cellular = TRUE, irrigation = FALSE, aggregate = FALSE)
+      kcr2all      <- data.frame(list(kcr = getNames(SOCLossShare), all = rep("all", 19)))
+      SOCLossShare <- toolAggregate(SOCLossShare, weight = MAGCrop, rel = kcr2all, from = "kcr", to = "all", dim = 3)
+      getNames(SOCLossShare) <- "cshare"
     }
   }
 
   if (rate == "loss") {
-    socLossShare <- 1 - socLossShare
+    SOCLossShare <- 1 - SOCLossShare
   } else if (rate != "change") {
     stop(paste(rate, "is unknown as 'rate' parameter specification."))
   }
 
-  weight <- dimSums(calcOutput("LanduseInitialisation", aggregate = FALSE, cellular = TRUE,
-                               input_magpie = FALSE, years = "y1995", round = 6), dim = 3)
+  weight <- dimSums(calcOutput("LanduseInitialisation", aggregate = FALSE, cellular = TRUE, input_magpie = FALSE, years = "y1995", round = 6), dim = 3)
 
   return(list(
-    x            = socLossShare,
+    x            = SOCLossShare,
     weight       = weight,
     unit         = "tC/tC",
     description  = "Soil organic carbon loss share per crop type",
