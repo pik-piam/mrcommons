e5d3b6fbe4d0de0d0042654752f4ec82e28c6841
Kristine Karstens <karstens@pik-potsdam.de>
2023-03-06 22:21:42 +0100

    revert back to underscore

--- R/toolLPJmLVersion.R
+++ R/toolLPJmLVersion.R
@@ -16,13 +16,13 @@ toolLPJmLVersion <- function(version, climatetype) {
   cfg <- NULL
 
   ##### DEFAULT CONFIG #####
-  cfg$baselineHist    <- "GSWP3-W5E5:historical"
-  cfg$refYearHist     <- "y2010"
-  cfg$baselineGcm     <- "MRI-ESM2-0:ssp370"
-  cfg$refYearGcm      <- "y2020"
-  cfg$readinVersion   <- version
-  cfg$baselineVersion <- version
-  cfg$climatetype     <- climatetype
+  cfg$baseline_hist    <- "GSWP3-W5E5:historical"
+  cfg$ref_year_hist    <- "y2010"
+  cfg$baseline_gcm     <- "MRI-ESM2-0:ssp370"
+  cfg$ref_year_gcm     <- "y2020"
+  cfg$readin_version   <- version
+  cfg$baseline_version <- version
+  cfg$climatetype      <- climatetype
   ##### DEFAULT CONFIG #####
 
 
@@ -31,11 +31,11 @@ toolLPJmLVersion <- function(version, climatetype) {
   # (1) add-on tag in version argument - implemented add-ons:
   # * `+oldGSWP3`               - use older LPJmL version for GSWP3
   #                               as long as new GSWP3 is not available
-  # * `+baselineGcm<GCM:RCP>`  - use another baseline for 2010--2020
-  # * `+baselineHist<GCM:RCP>` - use another baseline for 1965--2010
+  # * `+baseline_gcm<GCM:RCP>`  - use another baseline for 2010--2020
+  # * `+baseline_hist<GCM:RCP>` - use another baseline for 1965--2010
   #
   # (2) add-on tag in climatetype argument - implemented add-ons:
-  # * `_<scenarioAddon>   - add scenario specification to baseline climatetypes
+  # * `_<scenario_addon>   - add scenario specification to baseline climatetypes
 
   ### version addon
   if (grepl("\\+", version)) {
@@ -43,23 +43,23 @@ toolLPJmLVersion <- function(version, climatetype) {
 
     if (any(tmp == "oldGSWP3")) {
       if (climatetype == "GSWP3-W5E5:historical") {
-        if (grepl("LPJmL4", tmp[1])) cfg$readinVersion <- "LPJmL4_for_MAgPIE_84a69edd"
-        if (grepl("ggcmi", tmp[1]))  cfg$readinVersion <- "ggcmi_phase3_nchecks_fbed5c8b_newparam"
+        if (grepl("LPJmL4", tmp[1])) cfg$readin_version <- "LPJmL4_for_MAgPIE_84a69edd"
+        if (grepl("ggcmi", tmp[1]))  cfg$readin_version <- "ggcmi_phase3_nchecks_fbed5c8b_newparam"
       } else {
-        cfg$readinVersion <- tmp[1]
+        cfg$readin_version <- tmp[1]
       }
     }
 
-    if (any(grepl("baselineGcm", tmp))) {
-      i <- grep("baselineGcm", tmp)
-      cfg$baselineGcm   <- gsub("baselineGcm", "", tmp[i])
-      cfg$readinVersion <- tmp[1]
+    if (any(grepl("baseline_gcm", tmp))) {
+      i <- grep("baseline_gcm", tmp)
+      cfg$baseline_gcm   <- gsub("baseline_gcm", "", tmp[i])
+      cfg$readin_version <- tmp[1]
     }
 
-    if (any(grepl("baselineHist", tmp))) {
-      i <- grep("baselineHist", tmp)
-      cfg$baselineHist <- gsub("baselineHist", "", tmp[i])
-      cfg$readinVersion   <- tmp[1]
+    if (any(grepl("baseline_hist", tmp))) {
+      i <- grep("baseline_hist", tmp)
+      cfg$baseline_hist  <- gsub("baseline_hist", "", tmp[i])
+      cfg$readin_version <- tmp[1]
     }
 
     ## Specific if in case the gsadapt scenario want to be harmonized to "standard" 2020
@@ -67,11 +67,11 @@ toolLPJmLVersion <- function(version, climatetype) {
 
     if (any(grepl("gsadapt2020", tmp))) {
 
-      if (cfg$climatetype != cfg$baselineHist) {
-                cfg$readinVersion <- paste0(tmp[1], "+scen:gsadapt")
-      } else {  cfg$readinVersion <- tmp[1] }
+      if (cfg$climatetype != cfg$baseline_hist) {
+                cfg$readin_version <- paste0(tmp[1], "+scen:gsadapt")
+      } else {  cfg$readin_version <- tmp[1] }
 
-      cfg$baselineVersion <- tmp[1]
+      cfg$baseline_version <- tmp[1]
     }
 
   }
