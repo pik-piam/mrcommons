b791b3a88889853738dbf74d5f7201bc99e5c13d
gabriel-abrahao <gabrielmabrahao@gmail.com>
2024-01-03 17:50:00 +0100

    Baseline independent convert function

---
+++ R/convertPBL_MACC_SSP2_2022.R
@@ -0,0 +1,19 @@
+#' Convert subtypes of the PBL_MACC_SSP2_2022 data
+#' 
+#' Convert subtypes from PBL_MACC_SSP2_2022 to data on ISO country level.#' 
+#' 
+#' @param x MAgPIE object containing PBL_MACC_SSP2_2022 data on region level
+#' @return PBL_MACC_SSP2_2022 data as MAgPIE object for all subtypes aggregated to
+#' country level
+#' @author Michael Windisch, Florian Humpenoeder
+#' @seealso [readSource()]
+
+convertPBL_MACC_SSP2_2022 <- function(x) {
+  
+  map <- toolGetMapping(type = "regional", name = "regionmapping_IMAGE_PBL_MACC_2019.csv")
+  
+   y <- toolAggregate(x,map,from = "RegionCode",to = "CountryCode")
+   y <- toolCountryFill(y,fill = 0)
+
+  return(y)
+}
