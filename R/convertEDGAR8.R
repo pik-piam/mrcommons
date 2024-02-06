9834293753398ec202f7d4e1b5c6d5ec09cf7a3d
Pascal Weigmann <pascal.weigmann@pik-potsdam.de>
2024-02-06 17:17:35 +0100

    add read and convert for EDGAR8

---
+++ R/convertEDGAR8.R
@@ -0,0 +1,44 @@
+convertEDGAR8 <- function() {
+  x <- readSource("EDGAR8", convert =F)
+
+  # rename regions
+  getItems(x, dim = 1) <- sub(pattern = "ANT", replacement = "SXM",
+                              x = getItems(x, dim = 1))
+  getItems(x, dim = 1) <- sub(pattern = "SCG", replacement = "SRB",
+                              x = getItems(x, dim = 1))
+
+  # Analog to convertCEDS: shipping and aviation data is global only
+  # We want to distribute it evenly across all countries.
+  # Therefore, save global data because it will be removed by toolCountryfill
+
+  # sum all sub-sectors of int. aviation and shipping per pollutant
+  air <- dimSums(x["AIR", , ], dim = 3.2, na.rm = TRUE) %>%
+    add_dimension(dim = 3.2, add = "variable", nm = "Aviation")
+  getItems(air, dim = 1) <- "GLO"
+
+  sea <- dimSums(x["SEA", , ], dim = 3.2, na.rm = TRUE) %>%
+    add_dimension(dim = 3.2, add = "variable", nm = "Shipping")
+  getItems(sea, dim = 1) <- "GLO"
+
+  # combine bunker data
+  bunkers <- mbind(air, sea)
+
+  # fills missing ISO countries and remove unknown ISO countries and bunkers
+  x <- toolCountryFill(x, fill = 0)
+
+  # create weight 1 for bunkers
+  w <- new.magpie(getItems(x, dim = 1),
+                  getItems(x, dim = 2),
+                  getItems(bunkers, dim = 3),
+                  fill = 1)
+
+  # create mapping of each country to GLO
+  m <- data.frame(from = getItems(x, dim = 1), to = "GLO")
+
+  # spread global shipping and aviation data evenly across countries and
+  # save it to regions of x
+  x <- add_columns(x, addnm = c("Aviation", "Shipping"), dim = 3.2, fill = NA)
+  x[, , getItems(bunkers, dim = 3)] <- toolAggregate(bunkers, m, weight = w)
+
+  return(x)
+}
