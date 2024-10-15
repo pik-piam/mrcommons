1a4e768fd4510ab016106895ae789029c3693adf
florianh <humpenoeder@pik-potsdam.de>
2024-10-15 15:30:08 +0200

    bugfix warning

--- R/convertIMAGE.R
+++ R/convertIMAGE.R
@@ -2,6 +2,7 @@ convertIMAGE <- function(x) {
   w <- readSource("EDGAR", subtype = "HFC")
   w[is.na(w)] <- 0
   w <- dimSums(w[, 2005, ], dim = 3)["ATA", , , invert = TRUE]
+  w <- w + 10^-10
   y <- toolAggregate(x, "regionmappingIMAGE.csv", weight = w)
   y <- toolCountryFill(y, fill = 0)
   return(y)
