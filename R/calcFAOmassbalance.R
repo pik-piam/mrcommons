d8925b7fd4474db834936177a26f8c11b9b9a4e2
caviddhen <david.chen@pik.potsdam.de>
2024-10-17 23:26:57 +0200

    add rounding to massbalance

--- R/calcFAOmassbalance.R
+++ R/calcFAOmassbalance.R
@@ -108,6 +108,8 @@ calcFAOmassbalance <- function() {
             "oils")][, , "other_util"] <- mb3[, , c("ethanol",
                                                     "oils")][, , "other_util"] - mb3[, , c("ethanol",
                                                                                            "oils")][, , "bioenergy"]
+  #round to 1 ton to avoid calculation issues
+  mb3 <- round(mb3, 6)
 
   return(list(
               x = mb3,
