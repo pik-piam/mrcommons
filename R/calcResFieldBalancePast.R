60ba5d22cc4fdbd8b2aa4b8d2ab5477006941bdb
caviddhen <david.chen@pik.potsdam.de>
2025-04-22 14:42:17 +0200

    update resfieldbalancepast

--- R/calcResFieldBalancePast.R
+++ R/calcResFieldBalancePast.R
@@ -19,14 +19,13 @@
 calcResFieldBalancePast <- function(cellular = FALSE, products = "sum", scenario = "default") {
 
   if (products == "kres") {
-    past              <- findset("past_til2020")
     relevantNutrients <- c("nr", "p", "k", "c")  # after burning, unclear what dm and ge may be
 
     production        <- collapseNames(calcOutput("ResBiomass", cellular = cellular, plantparts = "ag",
-                                                  aggregate = FALSE, scenario = scenario))[, past, relevantNutrients]
+                                                  aggregate = FALSE, scenario = scenario))[, , relevantNutrients]
 
     burnshr           <- calcOutput("ResCombustEff", aggregate = FALSE)[, , getNames(production, dim = 1)]
-    devStatePast      <- collapseNames(calcOutput("DevelopmentState", aggregate = FALSE)[, past, "SSP2"])
+    devStatePast      <- collapseNames(calcOutput("DevelopmentState", aggregate = FALSE)[, , "SSP2"])
     commonYears  <- intersect(getYears(production), getYears(devStatePast))
     devStatePast <- devStatePast[, commonYears, ]
     production   <- production [, commonYears, ]
@@ -78,6 +77,8 @@ calcResFieldBalancePast <- function(cellular = FALSE, products = "sum", scenario
       cell2Coord   <- toolGetMappingCoord2Country(pretty = TRUE)
       removalshare <- toolAggregate(x = removalshare, rel = cell2Coord,
                                     from = "iso", to = "coords", partrel = TRUE)
+      cyears <- intersect(getYears(production), getYears(removalshare))
+      removalshare <- removalshare[, cyears, ]
       removal <- (production - burn - ash) * removalshare
 
     } else {
