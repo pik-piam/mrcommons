b500e2185b177990cf06b76a2b5d32f347c2d96c
Isabelle Weindl <weindl@pik-potsdam.de>
2025-06-02 11:21:12 +0200

    introduced versioned input data in the read-function for livestock productivity assumptions

--- R/readExpertGuessSSPLivestockProduction.R
+++ R/readExpertGuessSSPLivestockProduction.R
@@ -9,7 +9,7 @@
 #'
 #'
 #' @return magpie object containing the expert guesses
-#' @author Stephen Wirth
+#' @author Stephen Wirth, Isabelle Weindl
 #' @seealso [madrat::readSource()]
 #' @examples
 #' \dontrun{
@@ -18,8 +18,10 @@
 
 
 readExpertGuessSSPLivestockProduction <- function(subtype) {
-
-
+  
+  # input data version
+  ver <- "2025-06"
+  
   files <- c(
     ssp1 = "animal_productivity_expert_guess_ssp1.csv",
     ssp2 = "animal_productivity_expert_guess_ssp2.csv",
@@ -27,9 +29,9 @@ readExpertGuessSSPLivestockProduction <- function(subtype) {
     ssp4 = "animal_productivity_expert_guess_ssp4.csv",
     ssp5 = "animal_productivity_expert_guess_ssp5.csv"
   )
-
+  
   file <- toolSubtypeSelect(subtype, files)
-
-  x <- read.magpie(file)
+  
+  x <- read.magpie(file.path(ver, file))
   return(x)
 }
