b8a8882a500a10810f3a4ac1cf7d9a098299f28f
caviddhen <david.chen@pik.potsdam.de>
2025-08-27 10:12:03 +0200

    re-indent and comment TimberTradeBilateral

--- R/calcTimberTradeBilateral.R
+++ R/calcTimberTradeBilateral.R
@@ -14,12 +14,12 @@
 #' @export
 
 calcTimberTradeBilateral <- function(products = "magpie") {
-
+  
   bilat <- readSource("FAOTradeMatrix", "import_qty_kforestry", convert = TRUE)
   # because the forestry matrix doesn't include many proeducts, including woodfuel completely,
   # we only take the sum of all products as general bilateral trade patterns to distribute the
   # unilateral trade across
-
+  
   ## first conver the tonnes to m3 as in calcTimberDemand
   bilat[, , c("1860|Paper and paperboard, excluding newsprint",
               "1875|Wood pulp", "1671|Newsprint")] <-
@@ -32,42 +32,42 @@ calcTimberTradeBilateral <- function(products = "magpie") {
   bilatShr <- bilat / dimSums(bilat, dim  = 1.2)
   bilatShr[is.na(bilatShr)] <- 0
   timbDem <- calcOutput("TimberDemand", aggregate = FALSE)
-
+  
   # hold future constant but remove the past
   yearsExt <- setdiff(getYears(timbDem),
                       getYears(bilatShr))[which(setdiff(getYears(timbDem, as.integer = TRUE),
                                                         getYears(bilatShr, as.integer = TRUE)) > 2015)]
   bilatShr <- toolHoldConstant(bilatShr, years = yearsExt)
   cyears <- intersect(getYears(bilatShr), getYears(timbDem))
-
-
+  
+  
   # rename exporters so that we distribute the  the importers across their own supply
   getItems(bilatShr, dim = 1.2) <- paste0(getItems(bilatShr, dim = 1.2), "2")
-
+  
   # multiply
   out <- timbDem[, cyears, "import", drop = TRUE] * bilatShr
   getItems(out, dim = 1.2) <- gsub("[0-9]+", "", getItems(out, dim = 1.2))
   out <- collapseNames(out)
-
+  
   getSets(out)[c(1, 2)] <- c("im", "ex")
   out[is.na(out)] <- 0
   out[is.infinite(out)] <- 0
   unit <- "mio m3"
-
-  if (products == "magpie") {
-    
-  out[, , "Industrial roundwood"] <- out[, , "Industrial roundwood"] * 0.6
-  out <- add_columns(out, dim = 3, addnm = "wood")
-  out[, , "wood"] <- out[, , "Industrial roundwood"] 
-
-  out[, , "Wood fuel"] <- out[, , "Wood fuel"] * 0.3
-  out <- add_columns(out, dim = 3, addnm = "woodfuel")
-  out[, , "woodfuel"] <- out[, , "Wood fuel"] 
   
-  out <- out[, , c("wood", "woodfuel")]
-  unit <- "mio T"
+  if (products == "magpie") {
+    #convert to dry matter content, 0.6 and 03 respectively
+    out[, , "Industrial roundwood"] <- out[, , "Industrial roundwood"] * 0.6
+    out <- add_columns(out, dim = 3, addnm = "wood")
+    out[, , "wood"] <- out[, , "Industrial roundwood"] 
+    
+    out[, , "Wood fuel"] <- out[, , "Wood fuel"] * 0.3
+    out <- add_columns(out, dim = 3, addnm = "woodfuel")
+    out[, , "woodfuel"] <- out[, , "Wood fuel"] 
+    
+    out <- out[, , c("wood", "woodfuel")]
+    unit <- "mio T"
   }
-
+  
   return(list(
     x = out,
     weight = NULL,
