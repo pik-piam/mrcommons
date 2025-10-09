1933b6af34a749b96a2c6fdff29bd35db35c7990
Pascal Weigmann <pascal.weigmann@pik-potsdam.de>
2025-10-09 16:01:53 +0200

    update EDGARghg to 2025 release

--- R/convertEDGARghg.R
+++ R/convertEDGARghg.R
@@ -1,4 +1,4 @@
-convertEDGARghg <- function(x) {
+convertEDGARghg <- function(x, subtype) {
 
   # rename regions
   getItems(x, dim = 1) <- sub(pattern = "ANT", replacement = "SXM",
@@ -6,40 +6,48 @@ convertEDGARghg <- function(x) {
   getItems(x, dim = 1) <- sub(pattern = "SCG", replacement = "SRB",
                               x = getItems(x, dim = 1))
 
-  # Analog to convertCEDS: shipping and aviation data is global only
-  # We want to distribute it evenly across all countries.
-  # Therefore, save global data because it will be removed by toolCountryfill
+  if (subtype == "ghg_by_sector") {
+    # Analog to convertCEDS: shipping and aviation data is global only
+    # We want to distribute it evenly across all countries.
+    # Therefore, save global data because it will be removed by toolCountryfill
 
-  # sum all sub-sectors of int. aviation and shipping per pollutant
-  air <- dimSums(x["AIR", , ], dim = 3.2, na.rm = TRUE) %>%
-    add_dimension(dim = 3.2, add = "variable", nm = "Aviation")
-  getItems(air, dim = 1) <- "GLO"
+    # sum all sub-sectors of int. aviation and shipping per pollutant
+    air <- dimSums(x["AIR", , ], dim = 3.2, na.rm = TRUE) %>%
+      add_dimension(dim = 3.2, add = "variable", nm = "Aviation")
+    getItems(air, dim = 1) <- "GLO"
 
-  sea <- dimSums(x["SEA", , ], dim = 3.2, na.rm = TRUE) %>%
-    add_dimension(dim = 3.2, add = "variable", nm = "Shipping")
-  getItems(sea, dim = 1) <- "GLO"
+    sea <- dimSums(x["SEA", , ], dim = 3.2, na.rm = TRUE) %>%
+      add_dimension(dim = 3.2, add = "variable", nm = "Shipping")
+    getItems(sea, dim = 1) <- "GLO"
 
-  x <- x[c("AIR", "SEA"), , invert = TRUE]
+    x <- x[c("AIR", "SEA"), , invert = TRUE]
 
-  # combine bunker data
-  bunkers <- mbind(air, sea)
+    # combine bunker data
+    bunkers <- mbind(air, sea)
 
-  # fills missing ISO countries and remove unknown ISO countries and bunkers
-  x <- toolCountryFill(x, fill = 0, verbosity = 2)
+    # fills missing ISO countries and remove unknown ISO countries and bunkers
+    x <- toolCountryFill(x, fill = 0, verbosity = 2)
 
-  # create weight 1 for bunkers
-  w <- new.magpie(getItems(x, dim = 1),
-                  getItems(x, dim = 2),
-                  getItems(bunkers, dim = 3),
-                  fill = 1)
+    # create weight 1 for bunkers
+    w <- new.magpie(getItems(x, dim = 1),
+                    getItems(x, dim = 2),
+                    getItems(bunkers, dim = 3),
+                    fill = 1)
 
-  # create mapping of each country to GLO
-  m <- data.frame(from = getItems(x, dim = 1), to = "GLO")
+    # create mapping of each country to GLO
+    m <- data.frame(from = getItems(x, dim = 1), to = "GLO")
 
-  # spread global shipping and aviation data evenly across countries and
-  # save it to regions of x
-  x <- add_columns(x, addnm = c("Aviation", "Shipping"), dim = 3.2, fill = NA)
-  x[, , getItems(bunkers, dim = 3)] <- toolAggregate(bunkers, m, weight = w)
+    # spread global shipping and aviation data evenly across countries and
+    # save it to regions of x
+    x <- add_columns(x, addnm = c("Aviation", "Shipping"), dim = 3.2, fill = NA)
+    x[, , getItems(bunkers, dim = 3)] <- toolAggregate(bunkers, m, weight = w)
+
+  } else {
+
+    # lulucf data doesn't contain AIR and SEA as it doesn't contain bunkers
+    # fills missing ISO countries and remove unknown ISO countries
+    x <- toolCountryFill(x, fill = 0, verbosity = 2)
+  }
 
   return(x)
 }
--- R/readEDGARghg.R
+++ R/readEDGARghg.R
@@ -1,25 +1,51 @@
 #' @title readEDGAR_GHG
 #' @description read EDGAR GHG emission data
+#' @param subtype ghg_by_sector to read in the sheet GHG_by_sector_and_country,
+#'                lulucf to read in the sheet LULUCF_countries
 #' @return magpie objects of EDGAR historical emissions (MtCO2eq)
 #' @author Pascal Weigmann
 #' @importFrom readxl read_xlsx
 
-readEDGARghg <- function() {
+readEDGARghg <- function(subtype = "ghg_by_sector") {
 
-  file <- "EDGAR_2024_GHG_booklet_2024.xlsx"
-  sheet <- "GHG_by_sector_and_country"
+  file <- "EDGAR_2025_GHG_booklet_2025.xlsx"
 
-  # read in and clean source data and transform to magclass object
-  # hard-coded last relevant row, so in case the data file changes -> adapt
-  ed <- as.data.frame(read_xlsx(file, sheet = sheet, na = "NULL", n_max = 4801),
-                      stringsAsFactors = FALSE) %>%
-    rename(region = "EDGAR Country Code",
-           variable = "Sector",
-           pollutant = "Substance") %>%
-    select(-c("Country")) %>%
-    pivot_longer(cols = as.character(seq(1970, 2023)),
-                 names_to = "period") %>%
-    as.magpie()
+  if (subtype == "ghg_by_sector") {
+
+    sheet <- "GHG_by_sector_and_country"
+
+    # read in and clean source data and transform to magclass object
+    # hard-coded last relevant row, so in case the data file changes -> adapt
+    ed <- as.data.frame(read_xlsx(file, sheet = sheet, na = "NULL", n_max = 4798),
+                        stringsAsFactors = FALSE) %>%
+      rename(region = "EDGAR Country Code",
+             variable = "Sector",
+             pollutant = "Substance") %>%
+      select(-c("Country")) %>%
+      pivot_longer(cols = as.character(seq(1970, 2024)),
+                   names_to = "period") %>%
+      as.magpie()
+
+  } else if (subtype == "lulucf") {
+
+    sheet <- "LULUCF_countries"
+
+    # read in and clean source data and transform to magclass object
+    # hard-coded last relevant row, so in case the data file changes -> adapt
+    # "Net" variable not read in as it seems to be just the sum of all others
+    ed <- as.data.frame(read_xlsx(file, sheet = sheet, na = "NULL", n_max = 1067),
+                        stringsAsFactors = FALSE) %>%
+      rename(region = "EDGAR Country Code",
+             variable = "Sector",
+             pollutant = "Substance") %>%
+      select(-c("Country", "Macro-region")) %>%
+      pivot_longer(cols = as.character(seq(1990, 2024)),
+                   names_to = "period") %>%
+      as.magpie()
+
+  } else {
+    stop("Please choose the subtype `ghg_by_sector` or `lulucf`.")
+  }
 
   return(ed)
 }
