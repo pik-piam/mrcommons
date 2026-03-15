c37068c32e7b77f37489e9be16d304679a02e842
florianh <humpenoeder@pik-potsdam.de>
2026-03-15 17:07:44 +0100

    refactor growing stock functions

--- R/calcGrowingStockNRF.R
+++ R/calcGrowingStockNRF.R
@@ -3,36 +3,50 @@
 #' Calculates the growing stocks from FAO data for naturally regenerating forests
 #' i.e. primary forests and secondary forests.
 #'
+#' @param indicator Either "per_ha" (m3/ha, area-weighted) or "total" (Mm3, absolute).
 #' @return List of magpie objects with results on country level, weight on country level, unit and description.
-#' @author Abhijeet Mishra
+#' @author Abhijeet Mishra, Florian Humpenoeder
 #' @examples
 #' \dontrun{
-#' calcOutput("GrowingStockNRF", aggregate = TRUE)
+#' calcOutput("GrowingStockNRF", indicator = "per_ha", aggregate = TRUE)
+#' calcOutput("GrowingStockNRF", indicator = "total", aggregate = TRUE)
 #' }
 #'
 #' @importFrom magpiesets FRAnames
 #' @export
 
-calcGrowingStockNRF <- function() {
-  ## Read Growing Stock
+calcGrowingStockNRF <- function(indicator = "per_ha") {
   out <- readSource("FRA2025", subtype = "growing_stock", convert = TRUE)
-  x <- out[, , grep(pattern = "ha", x = getNames(out), value = TRUE)]
-  getNames(x) <- gsub(pattern = "gs_ha_", replacement = "", x = getNames(x))
-  getNames(x) <- FRAnames(getNames(x))
-  area <- readSource("FRA2025", "forest_area", convert = TRUE)
-  getNames(area) <- FRAnames(getNames(area))
-  vars <- intersect(getNames(area), getNames(x))
-  x <- x[, , vars]
-  weight <- area[, , vars]
 
-  x <- setYears(collapseNames(x[, "y2025", "Natural Forest"]), NULL)
-  weight <- setYears(collapseNames(weight[, "y2025", "Natural Forest"]), NULL)
+  if (indicator == "per_ha") {
+    x <- out[, , grep(pattern = "gs_ha_", x = getNames(out), value = TRUE)]
+    getNames(x) <- gsub(pattern = "gs_ha_", replacement = "", x = getNames(x))
+    getNames(x) <- FRAnames(getNames(x))
+    area <- readSource("FRA2025", "forest_area", convert = TRUE)
+    getNames(area) <- FRAnames(getNames(area))
+    vars <- intersect(getNames(area), getNames(x))
+    x <- x[, , vars]
+    weight <- area[, , vars]
+
+    x <- setYears(collapseNames(x[, "y2025", "Natural Forest"]), NULL)
+    weight <- setYears(collapseNames(weight[, "y2025", "Natural Forest"]), NULL)
+    unit <- "m3/ha"
+  } else if (indicator == "total") {
+    x <- out[, , grep(pattern = "gs_tot_", x = getNames(out), value = TRUE)]
+    getNames(x) <- gsub(pattern = "gs_tot_", replacement = "", x = getNames(x))
+    getNames(x) <- FRAnames(getNames(x))
+    x <- setYears(collapseNames(x[, "y2025", "Natural Forest"]), NULL)
+    weight <- NULL
+    unit <- "Mm3"
+  } else {
+    stop("Invalid indicator. Use 'per_ha' or 'total'.")
+  }
 
   return(list(
     x = x,
     weight = weight,
     min = 0,
-    unit = "m3/ha",
+    unit = unit,
     description = paste("Calculates Growing stocks in naturally regenerating forests",
                         "as reported by Forest Resources Assessment Data 2025.")
   ))
--- R/calcGrowingStockPlantations.R
+++ R/calcGrowingStockPlantations.R
@@ -1,43 +1,68 @@
 #' @title calcGrowingStockPlantations
 #' @description
-#' Calculates the growing stocks on FAO data.
+#' Calculates the growing stocks from FAO data for planted forests (plantations).
 #'
+#' @param indicator Either "per_ha" (m3/ha, area-weighted) or "total" (Mm3, absolute).
 #' @return List of magpie objects with results on country level, weight on country level, unit and description.
-#' @author Abhijeet Mishra
+#' @author Abhijeet Mishra, Florian Humpenoeder
 #' @examples
 #' \dontrun{
-#' calcOutput("GrowingStockPlantations", aggregate = TRUE)
+#' calcOutput("GrowingStockPlantations", indicator = "per_ha", aggregate = TRUE)
+#' calcOutput("GrowingStockPlantations", indicator = "total", aggregate = TRUE)
 #' }
 #'
 #' @importFrom magpiesets FRAnames
 #' @export
 
-calcGrowingStockPlantations <- function() {
-  ## Read Growing Stock
+calcGrowingStockPlantations <- function(indicator = "per_ha") {
   out <- readSource("FRA2025", subtype = "growing_stock", convert = TRUE)
-  x <- out[, , grep(pattern = "ha", x = getNames(out), value = TRUE)]
-  getNames(x) <- gsub(pattern = "gs_ha_", replacement = "", x = getNames(x))
-  getNames(x) <- FRAnames(getNames(x))
-  area <- readSource("FRA2025", "forest_area", convert = TRUE)
-  getNames(area) <- FRAnames(getNames(area))
-  vars <- intersect(getNames(area), getNames(x))
-  x <- x[, , vars]
-  weight <- area[, , vars] + 10^-10
 
-  xPlant <- setYears(collapseNames(x[, "y2025", "Plantations"]), NULL)
-  wPlant <- setYears(collapseNames(weight[, "y2025", "Plantations"]), NULL)
-  xPlanted <- setYears(collapseNames(x[, "y2025", "Managed Forest"]), NULL)
-  wPlanted <- setYears(collapseNames(weight[, "y2025", "Managed Forest"]), NULL)
+  if (indicator == "per_ha") {
+    x <- out[, , grep(pattern = "gs_ha_", x = getNames(out), value = TRUE)]
+    getNames(x) <- gsub(pattern = "gs_ha_", replacement = "", x = getNames(x))
+    getNames(x) <- FRAnames(getNames(x))
+    area <- readSource("FRA2025", "forest_area", convert = TRUE)
+    getNames(area) <- FRAnames(getNames(area))
+    vars <- intersect(getNames(area), getNames(x))
+    x <- x[, , vars]
+    weight <- area[, , vars] + 10^-10
 
-  # Fill missing plantation data with planted forest as fallback
-  missing <- which(xPlant == 0 & xPlanted > 0)
-  xPlant[missing] <- xPlanted[missing]
-  wPlant[missing] <- wPlanted[missing]
+    xPlant <- setYears(collapseNames(x[, "y2025", "Plantations"]), NULL)
+    wPlant <- setYears(collapseNames(weight[, "y2025", "Plantations"]), NULL)
+    xPlanted <- setYears(collapseNames(x[, "y2025", "Managed Forest"]), NULL)
+    wPlanted <- setYears(collapseNames(weight[, "y2025", "Managed Forest"]), NULL)
 
-  return(list(x = xPlant,
-              weight = wPlant,
+    # Fill missing plantation data with planted forest as fallback
+    missing <- which(xPlant == 0 & xPlanted > 0)
+    xPlant[missing] <- xPlanted[missing]
+    wPlant[missing] <- wPlanted[missing]
+
+    x <- xPlant
+    weight <- wPlant
+    unit <- "m3/ha"
+  } else if (indicator == "total") {
+    x <- out[, , grep(pattern = "gs_tot_", x = getNames(out), value = TRUE)]
+    getNames(x) <- gsub(pattern = "gs_tot_", replacement = "", x = getNames(x))
+    getNames(x) <- FRAnames(getNames(x))
+
+    xPlant <- setYears(collapseNames(x[, "y2025", "Plantations"]), NULL)
+    xPlanted <- setYears(collapseNames(x[, "y2025", "Managed Forest"]), NULL)
+
+    # Fill missing plantation data with planted forest as fallback
+    missing <- which(xPlant == 0 & xPlanted > 0)
+    xPlant[missing] <- xPlanted[missing]
+
+    x <- xPlant
+    weight <- NULL
+    unit <- "Mm3"
+  } else {
+    stop("Invalid indicator. Use 'per_ha' or 'total'.")
+  }
+
+  return(list(x = x,
+              weight = weight,
               min = 0,
-              unit = "m3/ha",
-              description = "Calculates Growing stocks as reported by Forest Resources Assessment Data 2025."))
-
+              unit = unit,
+              description = "Calculates Growing stocks in plantations as reported by 
+              Forest Resources Assessment Data 2025."))
 }
